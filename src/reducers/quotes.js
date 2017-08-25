d3359b864b599012f4b019870634ed04318f067a
lucketta <lucketta@umich.edu>
2017-08-25 19:08:15 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [].concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      var index = state.findIndex(quote => quote.id === action.quoteId);
+      var quote = state[index];
+      return [...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)];
+    case 'DOWNVOTE_QUOTE':
+      var index = state.findIndex(quote => quote.id === action.quoteId);
+      var quote = state[index];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(index + 1)
+        ];
+      }
+    default:
+      return state;
+  }
 }
