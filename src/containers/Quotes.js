e34aef45033852422bce6f3649692f6727ddd7ce
Ivan Pilot <ivanpilot@gmail.com>
2017-08-30 03:44:33 +0200

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import {bindActionCreators} from 'redux';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
-
+// debugger
   render() {
+    // debugger
     return (
       <div>
         <hr />
@@ -15,11 +18,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/* 
-                TODO: 
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {this.props.quotes.map(quote => <QuoteCard key={quote.id} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} removeQuote={this.props.removeQuote} quote={quote} />)}
             </div>
           </div>
         </div>
@@ -28,4 +27,18 @@ class Quotes extends Component {
   }
 }
 
-export default connect(null, {})(Quotes);
+const mapStateToProps = (state) => {
+  return ({
+    quotes: state.quotes
+  })
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    removeQuote: removeQuote,
+    upvoteQuote: upvoteQuote,
+    downvoteQuote: downvoteQuote
+  }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
-export default (state = [], action) => {
-  return state;
+export default function quotes(state = [], action){
+  // debugger
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return Object.assign([], [...state.concat(action.quote)]);
+    case 'REMOVE_QUOTE':
+      return Object.assign([], [...state.filter( quote => quote.id !== action.quoteId)]);
+    case 'UPVOTE_QUOTE':
+
+      var upQuote = state.find( item => item.id === action.quoteId);
+      upQuote.votes > 0 ? upQuote.votes += 1 : upQuote.votes = 1;
+      return Object.assign([], [...state.filter( quote => quote.id !== action.quoteId)].concat(upQuote));
+    case 'DOWNVOTE_QUOTE':
+      var downQuote = state.find( item => item.id === action.quoteId);
+      downQuote.votes > 0 ? downQuote.votes -= 1 : downQuote.votes = 0;
+      return Object.assign([], [...state.filter( quote => quote.id !== action.quoteId)].concat(downQuote));
+    default:
+      return state;
+  }
 }
