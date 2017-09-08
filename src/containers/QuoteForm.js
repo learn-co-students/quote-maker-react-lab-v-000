61e3058e5ea7c59eb4dcb3e191f483dcb0825b4d
brownielox <mrsmpython@gmail.com>
2017-09-08 15:27:58 -0400

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -8,60 +8,65 @@ export class QuoteForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      author: "",
+      content: ""
     }
   }
 
-  handleOnChange = event => {
-    // Handle Updating Component State
+  handleOnChange(event) {
+    this.setState({
+      name: event.target.value
+    });
   }
 
-  handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state 
-    // Pass quote object to action creator 
-    // Update component state to return to default state
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.add(this.state);
   }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 col-md-offset-2">
-            <div className="panel panel-default">
-              <div className="panel-body">
-                <form className="form-horizontal">
-                  <div className="form-group">
-                    <label htmlFor="content" className="col-md-4 control-label">Quote</label>
-                    <div className="col-md-5">
-                      <textarea 
-                        className="form-control"
-                        value={this.state.content}
-                      />
+    render() {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2">
+              <div className="panel panel-default">
+                <div className="panel-body">
+                  <form className="form-horizontal" onSubmit={event => this.handleOnSubmit(event)}>
+                    <div className="form-group">
+                      <label htmlFor="content" className="col-md-4 control-label">Quote</label>
+                      <div className="col-md-5">
+                        <textarea name="content"
+                          className="form-control"
+                          value={this.state.content}
+                          onChange={event => this.handleOnChange(event)}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="author" className="col-md-4 control-label">Author</label>
-                    <div className="col-md-5">
-                      <input 
-                        className="form-control"
-                        type="text"
-                        value={this.state.author}
-                      />
+                    <div className="form-group">
+                      <label htmlFor="author" className="col-md-4 control-label">Author</label>
+                      <div className="col-md-5">
+                        <input name="author"
+                          className="form-control"
+                          type="text"
+                          value={this.state.author}
+                          onChange={event => this.handleOnChange(event)}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                    <div className="form-group">
+                      <div className="col-md-6 col-md-offset-4">
+                        <button type="submit" className="btn btn-default">Add</button>
+                      </div>
                     </div>
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
-export default connect(null, {})(QuoteForm);
+  export default connect(null, {})(QuoteForm);
