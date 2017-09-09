fe5b0fcb15d57dfddb91ceed02574d1e42c34a05
Jesse L Lawson <homestarflyer@gmail.com>
2017-09-09 19:24:10 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,34 @@
 // TODO: Create action creators as defined in tests
+
+const ADD_QUOTE = "ADD_QUOTE"
+const REMOVE_QUOTE = "REMOVE_QUOTE"
+const UPVOTE_QUOTE = "UPVOTE_QUOTE"
+const DOWNVOTE_QUOTE = "DOWNVOTE_QUOTE"
+
+export const addQuote = quote => {
+  return {
+    type: ADD_QUOTE,
+    quote: Object.assign({}, quote, {votes: 0})
+  }
+}
+
+export const removeQuote = quoteId => {
+  return {
+    type: REMOVE_QUOTE,
+    quoteId: quoteId
+  }
+}
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: UPVOTE_QUOTE,
+    quoteId: quoteId
+  }
+}
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: DOWNVOTE_QUOTE,
+    quoteId: quoteId
+  }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,57 @@
+import uuid from 'uuid'
+
 export default (state = [], action) => {
+  let index;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      //reminder: state is an array of quote objects
+      // gotta grab the index first
+      index = state.findIndex(quote => quote.id === action.quoteId)
+      //then get the exact quote object using the index
+      quote = state[index]
+
+      //return the state array where we spread up to the index
+      return [
+        ...state.slice(0, index),
+      //then make an updated copy of the object we indexed
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+      //then spread the rest of the state after the object we indexed
+        ...state.slice(index + 1)
+      ]
+
+     //looks like this state = [..."front of state","mod object", ...'rest of state' ]
+
+    case 'DOWNVOTE_QUOTE':
+      //get the obj you're looking for :)
+      index = state.findIndex(quote => quote.id === action.quoteId)
+      quote = state[index]
+
+      //get portions of the state b4 and after the indexed object that you are modifying
+      let before = state.slice(0, index)
+      let after = state.slice(index + 1)
+
+      if (quote.votes > 0) {
+        return [
+          ...before,
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...after
+        ];
+      }
+
+      return state;
+
+    default:
+      return state;
+  }
+
+
   return state;
 }
