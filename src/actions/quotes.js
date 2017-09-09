7475882d5a2a2ea359600d09e8a595a082fa13bf
mikka <mikkanthrope@gmail.com>
2017-09-09 12:53:49 -0700

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,30 @@
-// TODO: Create action creators as defined in tests
+export function addQuote(quote){
+  return {
+    type: "ADD_QUOTE",
+    payload: quote
+  }
+}
+
+export function removeQuote(quote){
+  return {
+    type: "REMOVE_QUOTE",
+    payload: quote
+  }
+}
+
+
+export function upVote(quote){
+
+  return {
+    type: "UPVOTE",
+    payload: quote
+  }
+}
+
+export function downVote(quote){
+
+  return {
+    type: "DOWNVOTE",
+    payload: quote
+  }
+}
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
+
+  const QuoteCards = this.props.quotes.map((quote, index) => {
+      return <QuoteCard content={quote.content} author={quote.author} key={index} store={this.props.store} />
+    });
 
   render() {
     return (
@@ -15,11 +20,8 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/* 
-                TODO: 
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+/* Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes */
+              {QuoteCards}
             </div>
           </div>
         </div>
@@ -28,4 +30,20 @@ class Quotes extends Component {
   }
 }
 
-export default connect(null, {})(Quotes);
+
+const mapStateToProps = (state) => {
+  return {
+    quotes: state.quotes
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addQuote: addQuote,
+    removeQuote: removeQuote,
+    upVote: upVote,
+    downVote: downVote
+  }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
