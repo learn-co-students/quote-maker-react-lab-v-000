b70995b29b6823b28ad860bc5f07ddc43defc41a
Malki Davis <md01@me.com>
2017-09-12 01:22:20 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,17 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case "ADD_QUOTE":
+      return state.concat(action.quote)
+    case "REMOVE_QUOTE":
+      const quotes = state.filter(quote => quote.id !== action.quoteId )
+
+      console.log(quotes)
+      return state = quotes)
+    case "UPVOTE_QUOTE":
+      return state
+    case "DOWNVOTE_QUOTE":
+      return state
+    default:
+      return state
+  }
 }
