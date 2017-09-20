d7e1e0f259dc344a7188f6e023215c89342981a2
Luisa Scavo <luisascavo@gmail.com>
2017-09-20 17:40:47 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,20 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      const upvoteIndex = state.findIndex(quote => quote.id === action.quoteId)
+      state[upvoteIndex].votes += 1
+      return state;
+    case 'DOWNVOTE_QUOTE':
+      const downvoteIndex = state.findIndex(quote => quote.id === action.quoteId)
+      if(state[downvoteIndex].votes > 0){
+        state[downvoteIndex].votes -= 1
+      }
+      return state;
+    default:
+      return state;
+  }
 }
