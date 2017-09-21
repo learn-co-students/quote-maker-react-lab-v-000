c54ec33fee5cf82867814be54100f227930c3315
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-20 21:05:34 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      // return [].concat(state, action.quote)
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':  
+      // let idx = state.indexOf(action.quoteId)
+      // return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+      return state.filter(quote => quote.id !== action.quoteId)      
+    case 'UPVOTE_QUOTE':      
+      var idx = state.indexOf(action.quoteId)
+      return state.map((quote, index) => {
+        if (quote.id === action.quoteId) {
+          quote.votes += 1
+        }
+        return quote
+      })
+    case 'DOWNVOTE_QUOTE':
+      var idx = state.indexOf(action.quoteId)
+      return state.map((quote, index) => {
+        if (quote.id === action.quoteId && quote.votes > 0) {
+          quote.votes -= 1
+        }
+        return quote
+      })
+  
+    default:
+      return state;      
+  }
+
 }
