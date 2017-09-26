d1b64fde67aaf8fa849124ca4fb9c2abe24ebc43
kkterai <kkterai@gmail.com>
2017-09-25 22:40:25 -0700

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import * as actionCreators from '../actions/quotes';
 
-const QuoteCard = (props) => 
+const QuoteCard = ({ quote, actionCreators }) => 
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{quote.content}</p> 
+          <footer>- author <cite title="Source Title">{ quote.author }</cite></footer>
         </blockquote>
       </div>
       <div className="float-right"> 
         <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
           <button 
+            onClick={() => upvoteQuote(quote.id)}
             type="button" 
             className="btn btn-primary"
           >
             Upvote
           </button>
           <button 
+            onClick={() => downvoteQuote(quote.id)}
             type="button" 
             className="btn btn-secondary"
           >
             Downvote
           </button>
           <button 
+            onClick={() => removeQuote(quote.id)}
             type="button" 
             className="btn btn-danger"
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+         Votes: { quote.votes }
       </div>
     </div>
   </div>;
 
-export default QuoteCard;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ actionCreators }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(QuoteCard);
