cd803252835b6ae0eb7f5986ac4333b3d59f3b2c
J Fernandez <jffernan@comcast.net>
2017-09-30 23:00:09 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,19 @@
 export default (state = [], action) => {
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+    case "UPVOTE_QUOTE":
+      var quotes = [...state];
+      quotes.find(quote => quote.id === action.quoteId).votes++;
+      return quotes;
+    case "DOWNVOTE_QUOTE":
+      var quotes = [...state];
+      var quote = quotes.find(quote => quote.id === action.quoteId);
+      if(quote.votes > 0){ quote.votes--};
+      return quotes;
+    default:
   return state;
+  }
 }
