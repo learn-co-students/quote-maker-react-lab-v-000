549423e20da857d001e6133c6da2c7a51b96cc6c
christinechoi <smi77y@gmail.com>
2017-10-24 15:00:39 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -9,3 +9,26 @@ export function addQuote(quote) {
     quote
   }
 }
+
+export function removeQuote(quoteId) {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  }
+}
+
+
+export function upvoteQuote(quoteId) {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  }
+}
+
+
+export function downvoteQuote(quoteId) {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId
+  }
+}
