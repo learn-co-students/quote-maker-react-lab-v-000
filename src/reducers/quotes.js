1d3c73b4e139af5cb5ef75c1f18962d757ae2fc3
Pamela Bortnick <pbortnick@gmail.com>
2017-10-24 21:19:11 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
+import uuid from 'uuid';
+const id = uuid();
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      var index = state.findIndex(quote => quote.id === action.quoteId)
+      var quote = state[index]
+      return [Object.assign({}, quote, { votes: quote.votes += 1})]
+    case "DOWNVOTE_QUOTE":
+      var index = state.findIndex(quote => quote.id === action.quoteId)
+      var quote = state[index]
+      if (quote.votes === 0) {
+        return state
+      } else {
+        return [Object.assign({}, quote, { votes: quote.votes -= 1})]
+      }
+    default:
+      return state;
+  }
 }
