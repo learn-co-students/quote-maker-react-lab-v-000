3d44220d800a2ae2f5105e02ba0ea5adcc9627ab
dustin <dandersonsoftware@outlook.com>
2017-11-01 22:32:00 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      let indexUpvote = state.findIndex(quote => quote.id === action.quoteId)
+      let quoteUpvote = state[indexUpvote]
+      return [
+        ...state.slice(0, indexUpvote),
+        Object.assign({}, quoteUpvote, { votes: quoteUpvote.votes += 1 }),
+        ...state.slice(indexUpvote + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      let indexDownvote = state.findIndex(quote => quote.id === action.quoteId)
+      let quoteDownvote = state[indexDownvote]
+      if (quoteDownvote.votes > 0) {
+        return [
+          ...state.slice(0, indexDownvote),
+          Object.assign({}, quoteDownvote, { votes: quoteDownvote.votes -= 1 }),
+          ...state.slice(indexDownvote + 1)
+        ];
+      }
+      return state;
+    default:
+    return state;
+  }
 }
