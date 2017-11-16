b1fd6a5d6cdd258392913083a66b2a38abb907d0
Moshe Levi <mo6709@gmail.com>
2017-11-16 14:15:14 -0500

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,3 +4,14 @@ import quotes from './quotes';
 export default combineReducers({
   quotes,
 });
+
+// export function combineReducers({quotes: quotes}){
+//   return (state = {}, action) => {
+//       return Object.keys(reducers).reduce(
+//          (nextState, key) => {
+//              nextState[key] = reducers[key](state[key], action);
+//              return nextState;
+//          }, {}
+//       )
+//   }
+// }
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [].concat(state, action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quete => quete.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      const uQuotes = state.map((quote) => {
+        if(quote.id === action.quoteId){
+          return Object.assign({}, quote, { votes: quote.votes += 1 });
+        }else{
+          return quote;
+        }
+      })
+      return uQuotes;
+    case 'DOWNVOTE_QUOTE':
+      const dQuotes = state.map((quote) => {
+        if(quote.votes > 0 && quote.id === action.quoteId){
+          return Object.assign({}, quote, { votes: quote.votes -= 1 });
+        }else{
+          return quote;
+        }
+      })
+      return dQuotes;
+    default:
+      return state;
+  }
 }
