8e48fde5adc849cb33984b204627dbd96311c70d
Aditi Green <asaki444@gmail.com>
2017-11-30 01:16:45 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+    switch (action.type) {
+ 
+     case "ADD_QUOTE":
+      return state.concat(action.quote);
+ 
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id != action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      let up_index = state.findIndex(quote => quote.id === action.quoteId)
+      state[up_index].votes++
+      return state
+
+      case 'DOWNVOTE_QUOTE':
+      let down_index = state.findIndex(quote => quote.id === action.quoteId)
+      if(state[down_index].votes>0){
+      state[down_index].votes--
+      }
+      return state
+ 
+    default:
+      return state;
+    }
+
 }
