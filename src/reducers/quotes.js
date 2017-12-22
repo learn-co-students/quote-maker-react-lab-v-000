4c01976e2a128f632a41b9d9d847f6c0c7a3a3ed
github username <github email address>
2017-12-22 01:13:27 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
-  return state;
+	switch(action.type) {
+		case 'ADD_QUOTE':
+			return [action.quote]
+		case 'REMOVE_QUOTE':
+			const quotes = state.filter(quote => 
+				quote.id !== action.quoteId)
+			return quotes
+		case 'UPVOTE_QUOTE':
+			return state.map(quote => {
+				if (quote.id === action.quoteId) {
+					quote.votes++;
+					return quote;
+				}
+			})
+		case 'DOWNVOTE_QUOTE':
+			return state.map(quote => {
+				if (quote.id === action.quoteId && quote.votes > 0) {
+					quote.votes--;
+					return quote;
+				} else {
+					return quote;
+				}
+			})
+		default:
+			return state;
+	}
 }
