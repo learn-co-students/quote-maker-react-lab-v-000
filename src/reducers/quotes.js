a61c1fc3b25f804fcc3ea3a9b04255693fcce99b
gb23 <gregbenj@hotmail.com>
2018-01-15 10:12:30 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,37 @@
 export default (state = [], action) => {
-  return state;
+  let index, quote;
+  switch (action.type) {
+  
+    case 'ADD_QUOTE': 
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      return state.filter(obj => obj.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(obj => obj.id === action.quoteId);
+      quote = state[index]
+
+      return [
+        ...state.slice(0, index), {...quote, votes: quote.votes += 1 },
+        ...state.slice(index + 1)
+      ];
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(obj => obj.id === action.quoteId);
+      quote = state[index]
+      if (quote.votes > 0){
+
+      return [
+        ...state.slice(0, index),
+        {...quote, votes: quote.votes -= 1 },
+        ...state.slice(index + 1)
+      ];
+    
+      } else {
+        return state;
+      }
+
+    default:
+      return state;
+  }
 }
