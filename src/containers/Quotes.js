3974b3f23579690d274311c1698c928ea3cde3da
Ynebuhs <shhubeny@gmail.com>
2018-01-29 20:25:13 -0700

    12 tests left to pass

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -5,6 +5,9 @@ import QuoteCard from '../components/QuoteCard';
 class Quotes extends Component {
 
   render() {
+    const quotes = this.props.quotes.map(quote => {
+      return <QuoteCard key={quote.id} quote={quote}/>
+    })
     return (
       <div>
         <hr />
@@ -15,11 +18,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/* 
-                TODO: 
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes}
             </div>
           </div>
         </div>
