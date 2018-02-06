60fc80f88bf196cb333b8c1b20e8ffddd390cd81
psnider30 <pbsnider@gmail.com>
2018-02-06 13:49:49 -0800

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
-export default (state = [], action) => {
-  return state;
+export default(state = [], action) => {
+  let idx;
+  let quote;
+
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      quote = Object.assign({}, state[idx], {votes: state[idx].votes + 1 })
+      return [ ...state.slice(0, idx), quote, ...state.slice(idx + 1) ];
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      if (state[idx].votes > 0) {
+        quote = Object.assign({}, state[idx], {votes: state[idx].votes - 1 })
+        return [ ...state.slice(0, idx), quote, ...state.slice(idx + 1) ];
+      } else {
+        return state;
+      }
+    default:
+      return state
+  }
 }
