8338b037d650d3842cfb2a80db085c8685be7ee1
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-07 05:54:17 +0000

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,41 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, {
+        author: action.quote.author,
+        content: action.quote.content,
+        id: action.quote.id,
+        votes: 0
+      }];
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      return state.map(quote => {
+        if (quote.id !== action.quoteId) {
+          return quote
+        }
+        return {
+          ...quote,
+          votes: quote.votes + 1
+        }
+      })
+    case 'DOWNVOTE_QUOTE':
+    return state.map(quote => {
+      if (quote.id !== action.quoteId) {
+        return quote
+      }
+      if (quote.votes !== 0) {
+        return {
+          ...quote,
+            votes: quote.votes - 1
+        }
+      } else {
+        return quote
+      }
+    })
+    default:
+      return state;
+  }
 }
