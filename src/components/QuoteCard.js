1562112fcfe977b4863fdc51c5ff80c67a49fa2a
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-11 00:44:24 -0800

    got quotes to display

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,30 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const QuoteCard = (props) => 
+const QuoteCard = (props) =>
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
+          <p>{props.quotes.length === 0 ? 'no quotes yet' : 'there are some quotes here'}</p>
           {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
         </blockquote>
       </div>
-      <div className="float-right"> 
+      <div className="float-right">
         <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button 
-            type="button" 
+          <button
+            type="button"
             className="btn btn-primary"
           >
             Upvote
           </button>
-          <button 
-            type="button" 
+          <button
+            type="button"
             className="btn btn-secondary"
           >
             Downvote
           </button>
-          <button 
-            type="button" 
+          <button
+            type="button"
             className="btn btn-danger"
           >
             <span aria-hidden="true">&times;</span>
@@ -35,4 +36,9 @@ const QuoteCard = (props) =>
     </div>
   </div>;
 
-export default QuoteCard;
+const mapStateToProps = (state) => {
+  let thing = { quotes: state.quotes };
+  return thing;
+}
+
+export default connect(mapStateToProps)(QuoteCard);
