6ef6db0efddfbafa513eb033913b5372fc127956
emhopkins <epoletto@lstreetc.com>
2018-02-16 17:22:52 -0600

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,28 @@
-// TODO: Create action creators as defined in tests
+
+export function addQuote(quote){
+  return {
+    type: "ADD_QUOTE",
+    payload: quote
+  }
+}
+
+export function removeQuote(quoteId){
+  return {
+    type: "REMOVE_QUOTE",
+    payload: quoteId
+  }
+}
+
+export function upvoteQuote(quoteId){
+  return {
+    type: "UPVOTE_QUOTE",
+    payload: quoteId
+  }
+}
+
+export function downvoteQuote(quoteId){
+  return {
+    type: "DOWNVOTE_QUOTE",
+    payload: quoteId
+  }
+}
