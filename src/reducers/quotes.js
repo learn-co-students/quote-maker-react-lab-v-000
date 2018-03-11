1f23a59590e9b16b4578fcf5bab9a04da7bbb248
¨anniezh12¨ <aniqaelahi@yahoo.com>
2018-03-10 22:46:59 -0800

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
 export default (state = [], action) => {
-  return state;
+
+ switch(action.type){
+
+   case 'ADD_QUOTE':
+   return state.concat(action.quote)
+   case 'REMOVE_QUOTE':
+   return state.filter(quote=> quote.id !== action.quoteId);
+   case 'UPVOTE_QUOTE':
+    let quote = state.find(q => q.id === action.quoteId)
+    quote.votes += 1;
+    return state.filter(q => q.id!==action.quoteId).concat(quote);//first filter the array(state) and remove the current quote and then re add it with a new vote value
+    case 'DOWNVOTE_QUOTE':
+     quote = state.find(q => q.id === action.quoteId) //find the current quote
+     if(quote.votes > 0){
+    quote.votes -= 1; //reduce the value of vote by 1
+    return state.filter(q => q.id !== action.quoteId).concat(quote);
+    }
+    return state;
+   default:
+   return state
+ }
+
 }
