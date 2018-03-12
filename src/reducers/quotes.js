392759fc8ce7774eb257171c690a5a821cc027c7
aback1227 <aback1227@gmail.com>
2018-03-12 10:03:04 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
+var votes;
 export default (state = [], action) => {
-  return state;
+  let quoteIndex, quote;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter((quote) => {return quote.id !== action.quoteId})
+    case 'UPVOTE_QUOTE':
+      quoteIndex = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[quoteIndex];
+      return [].concat(...state.slice(0, quoteIndex), {...quote, votes: quote.votes += 1 }, ...state.slice(quoteIndex+1))
+    case 'DOWNVOTE_QUOTE':
+      quoteIndex  = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[quoteIndex];
+      if (quote.votes > 0) {
+        return [].concat(...state.slice(0, quoteIndex), {...quote, votes: quote.votes -= 1}, ...state.slice(quoteIndex+1))
+      }   
+      return state;
+    default:
+      return state;
+  }
 }
