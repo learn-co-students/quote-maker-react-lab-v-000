756c903469654d55a35f965eef212bbae1701c0a
CLackrone <charisaanne@gmail.com>
2018-04-04 15:48:13 -0400

    App tests passing

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
 export default (state = [], action) => {
-  return state;
+  let newState;
+
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    default: 
+    return state;
+  }
+  
 }
+
+
+
+
+
+// export default function manageUsers(state = {users: [] }, action){
+//       console.log(action)
+
+//   switch(action.type) {
+//     case 'ADD_USER':
+//       const user = {username: action.user.username, hometown: action.user.hometown}
+//       return {users: [...state.users, user]}
+//     default: 
+//       return state
+//   }
+// }
