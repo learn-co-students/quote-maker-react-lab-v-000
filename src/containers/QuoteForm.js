3e15f9cb00a3b7e04e0d306b642a748c2087cf99
github username <github email address>
2018-05-08 17:26:09 -0700

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { addQuote } from '../actions/quotes';
+import { bindActionCreators } from 'redux';
 
 export class QuoteForm extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
+      content: '',
+      author: ''
     }
   }
-
   handleOnChange = event => {
-    // Handle Updating Component State
-  }
+  const {name,value} = event.target;
+  this.setState({
+    [name]: value
+  })
+}
 
   handleOnSubmit = event => {
+    event.preventDefault();
+    const quote = {
+      id: uuid(),
+      content: this.state.content,
+      author: this.state.author,
+      votes: 0
+    };
+    this.props.addQuote(quote);
+    this.setState({
+      content:'',
+      author: ''
+    })
     // Handle Form Submit event default
-    // Create quote object from state 
-    // Pass quote object to action creator 
+    // Create quote object from state
+    // Pass quote object to action creator
     // Update component state to return to default state
   }
 
@@ -29,29 +46,35 @@ export class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form
+                  className="form-horizontal"
+                  onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
-                      <textarea 
+                      <textarea
+                        name="content"
                         className="form-control"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
                   <div className="form-group">
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
-                      <input 
+                      <input
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                      <button onClick={this.handleOnSubmit} type="submit" className="btn btn-default">Add</button>
                     </div>
                   </div>
                 </form>
@@ -63,5 +86,9 @@ export class QuoteForm extends Component {
     );
   }
 }
-
-export default connect(null, {})(QuoteForm);
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addQuote
+  }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(QuoteForm);
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote } from '../actions/quotes';
+import { upvoteQuote } from '../actions/quotes';
+import { downvoteQuote } from '../actions/quotes';
+import { bindActionCreators } from 'redux';
 
 class Quotes extends Component {
 
@@ -15,11 +19,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/* 
-                TODO: 
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {this.props.quotes.map((quote, key) => <QuoteCard key={key} quote={quote} removeQuote={this.props.removeQuote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} />)}
             </div>
           </div>
         </div>
@@ -27,5 +27,14 @@ class Quotes extends Component {
     );
   }
 }
-
-export default connect(null, {})(Quotes);
+function mapStateToProps(state){
+  return {quotes: state.quotes}
+}
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    removeQuote,
+    upvoteQuote,
+    downvoteQuote
+  }, dispatch );
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
