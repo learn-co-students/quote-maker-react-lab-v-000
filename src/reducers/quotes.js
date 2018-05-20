91924c7d8a6a47c7867672308c7885385b7d370a
ingridwong0715 <ingrid830715@gmail.com>
2018-05-20 15:42:22 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+    case 'REMOVE_QUOTE':
+      return state.filter( quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      let upQuote = state.find(quote => quote.id === action.quoteId);
+      upQuote.votes++
+      const allOthers = state.filter(quote => quote.id !== action.quoteId);
+      allOthers.push(upQuote)
+      return allOthers;
+    case 'DOWNVOTE_QUOTE':
+      let quote = state.find( quote => quote.id === action.quoteId)
+      if (quote.votes > 0){quote.votes-- }
+      let mehAllOthers = state.filter( quote => quote.id !== action.quoteId);
+      mehAllOthers.push(quote);
+      return mehAllOthers
+
+    default:
+      return state
+  }
 }
