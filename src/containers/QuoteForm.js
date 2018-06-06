2cc409258f6d3accbf3ebb1ba6499573b087d063
jessiehuff <huffjd1@gmail.com>
2018-06-06 17:01:03 -0400

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -8,14 +8,24 @@ export class QuoteForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      content: '', 
+      author: ''
     }
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    this.setState({
+      [event.target.name]: event.target.value  
+    })
   }
 
   handleOnSubmit = event => {
+    event.preventDefault(); 
+    this.props.addQuote(Object.assign({}, this.state, {id: uuid() }))
+    this.setState({
+      content: '', 
+      author: ''
+    }); 
     // Handle Form Submit event default
     // Create quote object from state 
     // Pass quote object to action creator 
@@ -29,13 +39,15 @@ export class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea 
                         className="form-control"
+                        name="content"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -45,7 +57,9 @@ export class QuoteForm extends Component {
                       <input 
                         className="form-control"
                         type="text"
+                        name="author"
                         value={this.state.author}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
