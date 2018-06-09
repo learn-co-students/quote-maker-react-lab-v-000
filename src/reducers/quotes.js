cff1ea1bd1cc48dbaa59ca496b83c70127c3026d
meq <fewell@gmail.com>
2018-06-09 14:09:54 -0400

    working on it

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,22 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+let index
+let quote
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      quote = action.quote
+      quote.id = uuid()
+      return state.concat(quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+
+    case 'DOWNVOTE_QUOTE':
+
+    default:
+      return state;
+    }
 }
