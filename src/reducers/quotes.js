0bd2c78024213d1af780c97fa5b339b14dbbeec9
Penguino <heriberto@melo.nyc>
2018-06-11 19:31:23 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
 export default (state = [], action) => {
-  return state;
+  let quote;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return state.concat(action.quote);
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      quote = state.filter(quote => quote.id === action.quoteId)
+      quote[0].votes += 1
+      return quote
+    case "DOWNVOTE_QUOTE":
+    quote = state.filter(quote => quote.id === action.quoteId)
+    if (quote[0].votes !== 0){
+      quote[0].votes -= 1;
+      return quote
+    }
+    default:
+      return state;
+  }
 }
