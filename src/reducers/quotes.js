5bd1d82c741bb49ad1025ebd76935900e50c0356
rockychiang <rocky@chem.ucla.edu>
2018-06-12 00:03:55 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
+  let quote;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return Object.assign([], state.concat(action.quote));
+
+    case 'REMOVE_QUOTE':
+      return Object.assign([], state.filter(quote => quote.id !== action.quoteId));
+
+    case 'UPVOTE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId);
+      quote.votes++
+      return state;
+
+    case 'DOWNVOTE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId);
+      if (quote.votes > 0) {quote.votes-- }
+      return state;
+
+    default:
+      return state;
+  }
 }
