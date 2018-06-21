ef37896178a7774fcc1733c3e96b8d92d073d50d
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-06-20 20:54:49 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,29 @@
 // TODO: Create action creators as defined in tests
+
+export function addQuote(quote) {
+  return {
+    type: "ADD_QUOTE",
+    quote: Object.assign({}, quote, { votes: 0 })
+  }
+}
+
+export function removeQuote(id) {
+  return {
+    type: "REMOVE_QUOTE",
+    quoteId: id,
+  }
+}
+
+export function upvoteQuote(id) {
+  return {
+    type: "UPVOTE_QUOTE",
+    quoteId: id,
+  }
+}
+
+export function downvoteQuote(id) {
+  return {
+    type: "DOWNVOTE_QUOTE",
+    quoteId: id,
+  }
+}
