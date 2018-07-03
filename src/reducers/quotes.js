e27d20524615df37bda77ba3c17ed15e9c007b1a
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-07-03 11:57:02 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,9 +1,6 @@
 
 
 export default (state = [], action) => {
-  let index;
-  let quote;
-  
   switch (action.type) {
     case 'ADD_QUOTE':
       return state.concat([action.quote])
@@ -12,27 +9,30 @@ export default (state = [], action) => {
     case 'REMOVE_QUOTE':
       const newState = state.filter((quote) => quote.id !== action.quoteId)
       return newState
-    case 'UPVOTE_QUOTE':
-       index = state.findIndex(quote => quote.id === action.quoteId);
-       quote = state[index];
-
-       return [
-         ...state.slice(0, index),
-         Object.assign({}, quote, { votes: quote.votes += 1 }),
-         ...state.slice(index + 1)
-       ];
-
-     case 'DOWNVOTE_QUOTE':
-       index = state.findIndex(quote => quote.id === action.quoteId);
-       quote = state[index];
-       if (quote.votes > 0) {
-         return [
-           ...state.slice(0, index),
-           Object.assign({}, quote, { votes: quote.votes -= 1 }),
-           ...state.slice(index + 1)
-         ];
-       }
-       return state;
+     case 'UPVOTE_QUOTE':
+       // const index = state.findIndex((quote) => quote.id === action.quoteId)
+       // const thing = state[index]
+       //
+       // return [...state.slice(0, index),
+       // Object.assign({}, thing, {votes: thing.votes += 1}),
+       // ...state.slice(index+1)]
+       const quotes = state.map((quote) =>{
+         if (quote.id === action.quoteId){
+          return  Object.assign({}, quote, {votes: quote.votes += 1})
+         } else {
+           return quote
+         }
+       })
+       return quotes
+      case 'DOWNVOTE_QUOTE':
+        const quotes1 = state.map((quote) =>{
+          if (quote.id === action.quoteId && quote.votes > 0){
+           return  Object.assign({}, quote, {votes: quote.votes -= 1})
+          } else {
+            return quote
+          }
+        })
+        return quotes1
     default:
       return state;
   }
