16517d7d04d8e0e1577012d626ffa16cbdb845c5
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-07-26 23:46:40 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,36 @@
 export default (state = [], action) => {
-  return state;
+  let quoteIndex;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [].concat(state, action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      quoteIndex = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[quoteIndex];
+      return [
+        ...state.slice(0, quoteIndex), 
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(quoteIndex + 1, quoteIndex.length)
+      ];
+    case 'DOWNVOTE_QUOTE':
+      quoteIndex = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[quoteIndex];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, quoteIndex), 
+          Object.assign({}, quote, { 
+            votes: quote.votes -= 1 
+          }),
+          ...state.slice(quoteIndex + 1, quoteIndex.length)
+        ]
+      } else {
+          return state;
+      };
+      
+    default:
+      return state;
+  }
 }
