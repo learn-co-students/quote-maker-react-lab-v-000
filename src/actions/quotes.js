79a8fba00a5c70605def74ef7c20597d7923ddf1
Kelsey Fecho <kfecho30@gmail.com>
2018-08-02 17:02:47 +0000

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,27 @@
-// TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: "ADD_QUOTE",
+    quote
+  }
+}
+
+export const removeQuote = quoteId => {
+  return {
+    type: "REMOVE_QUOTE",
+    quoteId
+  }
+}
+
+export function upvoteQuote(quoteId){
+  return {
+    type: "UPVOTE_QUOTE",
+    quoteId
+  }
+}
+
+export function downvoteQuote(quoteId){
+  return {
+    type: "DOWNVOTE_QUOTE",
+    quoteId
+  }
+}
