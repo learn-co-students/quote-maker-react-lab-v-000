9a0271cf5ffe3b2337950b3b0ff8c3c701bc69b9
kristinreddington <kristinreddington@gmail.com>
2018-08-09 18:41:17 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,37 @@
-export default (state = [], action) => {
-  return state;
+
+export default function quotes(state = [], action)  {
+  let newState;
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+    return state.concat(action.quote)
+
+    case 'REMOVE_QUOTE':
+    return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+     newState = state.map(quote => {
+      if (quote.id === action.quoteId) {
+        return {...quote, votes: ++quote.votes}
+      } else {
+        return quote
+      }
+    })
+    return newState;
+
+    case 'DOWNVOTE_QUOTE':
+     newState = state.map(quote => {
+      if (quote.id === action.quoteId && quote.votes > 0) {
+        return {...quote, votes: --quote.votes}
+      } else {
+        return quote
+      }
+    })
+    return newState;
+
+    default:
+    return state;
+  }
+
+
 }
