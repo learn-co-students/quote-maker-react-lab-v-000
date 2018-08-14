ebcfd9d027ff0fc21c505a9fbcc28cde98d65e86
ecssiah <ecssiah@gmail.com>
2018-08-13 21:17:09 -0500

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const QuoteCard = (props) =>
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+class QuoteCard extends Component {
+  handleRemove = event => {
+    this.props.dispatch(this.props.removeQuote(this.props.quote.id)) 
+  }
+
+  handleUpvote = event => {
+    this.props.dispatch(this.props.upvoteQuote(this.props.quote.id)) 
+  }
+
+  handleDownvote = event => {
+    this.props.dispatch(this.props.downvoteQuote(this.props.quote.id)) 
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="card card-inverse card-success card-primary mb-3 text-center">
+          <div className="card-block">
+            <blockquote className="card-blockquote">
+              {<p>{this.props.quote.content}</p>}
+              {
+                <footer>
+                  - author <cite title="Source Title"> {this.props.quote.author}</cite>
+                </footer>
+              }
+            </blockquote>
+          </div>
+          <div className="float-right">
+            <div 
+              className="btn-group btn-group-sm" 
+              role="group" 
+              aria-label="Basic example"
+            >
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={this.handleUpvote}
+              >
+                Upvote
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={this.handleDownvote}
+              >
+                Downvote
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={this.handleRemove}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            {<div>Votes: {this.props.updateVotes(this.props.quote.id)}</div>}
+          </div>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
       </div>
-    </div>
-  </div>;
+    )
+  }
+}
 
-export default QuoteCard;
+export default connect()(QuoteCard);
