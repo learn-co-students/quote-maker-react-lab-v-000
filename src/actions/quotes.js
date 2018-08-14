192539603655a997af809fc7dafff35edfaf7e44
seanoughton <seanoughton@gmail.com>
2018-08-14 15:26:39 -0700

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,31 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  return {
+    type: 'ADD_QUOTE',
+    quote
+  };
+};
+
+  //should return an Object with a type of "REMOVE_QUOTE" and a quoteId
+export const removeQuote = (quoteId) => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  };
+};
+
+//should return an Object with a type of "UPVOTE_QUOTE" and a quoteId:
+export const upvoteQuote = (quoteId) => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  };
+};
+
+//should return an Object with a type of "DOWNVOTE_QUOTE" and a quoteId:
+export const downvoteQuote = (quoteId) => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId
+  };
+};
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,44 @@
 export default (state = [], action) => {
-  return state;
+/**
+export default (state = {
+  quotes: []
+}, action) => {
+  //return state; **/
+
+//let index;
+let quote;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      //return state.concat(action.quote);
+      return [...state,action.quote]
+
+    case 'REMOVE_QUOTE':
+      let id = action.quoteId
+      return state.filter(quote => quote.id !== id)
+
+    case 'UPVOTE_QUOTE':
+      //let quote = state.filter(quote => quote.id === action.quoteId)
+      quote = state.find(quote => quote.id === action.quoteId)
+      quote.votes += 1
+      return [quote]
+
+      case 'DOWNVOTE_QUOTE':
+        //let quote = state.filter(quote => quote.id === action.quoteId)
+        quote = state.find(quote => quote.id === action.quoteId)
+        if (quote.votes > 0) {
+          quote.votes -= 1
+        }
+
+        return [quote]
+
+
+
+    default:
+      return state;
+
+  }
+
+
 }
