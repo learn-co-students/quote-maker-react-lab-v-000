bb7671ca194fe46912af0c637f4a4397c88b9226
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-09-01 21:56:11 -0400

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeQuote } from '../actions/quotes';
+import { upvoteQuote } from '../actions/quotes';
+import { downvoteQuote } from '../actions/quotes';
 
 const QuoteCard = (props) =>
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{props.content}</p>
+          <footer>- author <cite title="Source Title">{props.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
@@ -14,25 +18,42 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
+            onClick={props.upvoteQuote}
           >
             Upvote
           </button>
           <button
             type="button"
             className="btn btn-secondary"
+            onClick={props.downvoteQuote}
           >
             Downvote
           </button>
           <button
             type="button"
             className="btn btn-danger"
+            onClick={props.removeQuote}
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {props.votes}</div>
       </div>
     </div>
   </div>;
 
-export default QuoteCard;
+const mapDispatchToProps = dispatch => {
+  return {
+    removeQuote: (quoteId) => {
+      dispatch(removeQuote(quoteId))
+    },
+    upvoteQuote: (quoteId) => {
+      dispatch(upvoteQuote(quoteId))
+    },
+    downvoteQuote: (quoteId) => {
+      dispatch(downvoteQuote(quoteId))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(QuoteCard);
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -5,6 +5,7 @@ import QuoteCard from '../components/QuoteCard';
 class Quotes extends Component {
 
   render() {
+    let renderQuotes = this.props.quotes.map((quote, index) => <QuoteCard key={index} content={quote.content} author={quote.author}/>)
     return (
       <div>
         <hr />
@@ -15,11 +16,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {renderQuotes}
             </div>
           </div>
         </div>
@@ -28,5 +25,9 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return { quotes: state.quotes }
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps)(Quotes);
