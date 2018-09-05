fc866a7d1977feeab5f5e40c6825c2a5c311b768
TrevorSeitz <trevinla@gmail.com>
2018-09-05 15:33:21 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,43 @@
 export default (state = [], action) => {
-  return state;
-}
+  let index;
+  let quote;
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return state.concat(action.quote);
+    // return {
+    //   ...state,
+    //   quotes: [...state.quotes, action.quote]
+    // };
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+    // idx = state.quote.indexOf(action.id);
+    // return {
+    //   ...state,
+    //   quote: [state.authors.slice(0, idx), state.authors.slice(idx + 1)]
+    // };
+    case "UPVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: (quote.votes += 1) }),
+        ...state.slice(index + 1)
+      ];
+
+    case "DOWNVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: (quote.votes -= 1) }),
+          ...state.slice(index + 1)
+        ];
+      }
+      return state;
+
+    default:
+      return state;
+  }
+};
