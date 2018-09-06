da13f8c1a6e28fc420ef230d045a336038d693a9
Anthony <github email address>
2018-09-05 20:16:35 -0400

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React from "react";
 
-const QuoteCard = (props) =>
+const QuoteCard = props => (
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{props.content}</p>
+          <footer>
+            - author <cite title="Source Title">{props.author}</cite>
+          </footer>
         </blockquote>
       </div>
       <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+        <div
+          className="btn-group btn-group-sm"
+          role="group"
+          aria-label="Basic example"
+        >
           <button
             type="button"
             className="btn btn-primary"
+            onClick={props.upvoteQuote.bind(null, props.id)}
           >
             Upvote
           </button>
           <button
             type="button"
             className="btn btn-secondary"
+            onClick={props.downvoteQuote.bind(null, props.id)}
           >
             Downvote
           </button>
           <button
             type="button"
             className="btn btn-danger"
+            onClick={props.removeQuote.bind(null, props.id)}
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {props.votes}</div>
       </div>
     </div>
-  </div>;
+  </div>
+);
 
 export default QuoteCard;
