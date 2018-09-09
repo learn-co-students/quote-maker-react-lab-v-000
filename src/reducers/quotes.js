e07f5424904ff4380fe9bce6ebf247a52f57df90
Michael Hirschberger <mlhirschberger@gmail.com>
2018-09-08 21:57:39 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,44 @@
-export default (state = [], action) => {
-  return state;
+import uuid from 'uuid';
+
+const quotes = (state = [], action) => {
+  let idx;
+  let quote;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      return [ ...state, action.quote ];
+
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      return [ ...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[idx];
+      return [
+        ...state.slice(0, idx),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(idx + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[idx];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, idx),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(idx + 1)
+        ];
+      } else {
+        return state;
+      }
+    
+    default:
+      return state;
+
+  }
 }
+
+export default quotes
