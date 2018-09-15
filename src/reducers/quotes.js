d4b3c134b3aba14ce397e045090167827bf3926e
badlychadly <chadmontoya21@yahoo.com>
2018-09-14 19:52:16 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,18 @@
 export default (state = [], action) => {
+  let index
+
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+      case "UPVOTE_QUOTE":
+      // return
+    default:
+      return state
+  }
+
+
   return state;
 }
