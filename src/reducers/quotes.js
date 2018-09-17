2f5d3707f7300fe33d4f963a2e097d8ba8293902
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-17 16:25:20 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,34 @@
 export default (state = [], action) => {
-  return state;
+  let quote
+  let index
+  
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return state.filter(q => {
+        return q.id !== action.quoteId
+      })
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
+      case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes -= 1 }),
+        ...state.slice(index + 1)
+      ];
+    default:
+      return state; 
+  }
 }
