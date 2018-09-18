bda185a51beb4e8b2f15087664310aa024eb3c90
Joanne Lee <vernapii@gmail.com>
2018-09-18 23:07:35 +0900

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,19 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+    case "UPVOTE_QUOTE":
+      let newState = state.map( quote => {
+        if (quote.id === action.quote.id) {
+          return { ...quote, votes: ++quote.votes}
+        } else {
+          return quote
+        }
+      })
+      return newState;
+    default:
+    return state;
+  }
 }
