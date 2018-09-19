17e3e1694a397979fbb50303978ae7f43f8c5848
illusioninlost <rchang9999@yahoo.com>
2018-09-18 21:51:00 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,10 @@
 export default (state = [], action) => {
+  let index;
+  let quote;
+
+  switch (action.type){
+    case 'ADD_QUOTE':
+    return state.concat(action.quote)
+  }
   return state;
 }
