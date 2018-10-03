1717407ae85f241f2887edb71292c0a98b0c9215
unenlightened <adept_inept@yahoo.com>
2018-10-02 19:13:33 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,8 +1,17 @@
 // TODO: Create action creators as defined in tests
-export const addQuote = (quote: {}) => {
+
+//quote: {}
+export const addQuote = (quote) => {
   return {
     type: 'ADD_QUOTE',
     quote
   }
 }
 
+//quoteId: Integer
+export const removeQuote (quoteId) => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  }
+}
