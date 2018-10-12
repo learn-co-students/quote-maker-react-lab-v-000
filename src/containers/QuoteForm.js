addab02e636ddedd5487a80af9252a15c8f3e465
Gingertonic <bethmschofield@gmail.com>
2018-10-12 10:27:03 +0100

    Passing tests

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -37,7 +37,7 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={(e) => this.handleOnSubmit(e)}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
@@ -63,7 +63,7 @@ class QuoteForm extends Component {
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default" onClick={(e) => this.handleOnSubmit(e)}>Add</button>
+                      <button type="submit" className="btn btn-default">Add</button>
                     </div>
                   </div>
                 </form>
