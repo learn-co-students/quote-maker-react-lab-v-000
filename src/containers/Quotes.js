1bd0460b127d6cab2523d6a0d7f453e554e7148b
Gingertonic <bethmschofield@gmail.com>
2018-10-12 10:14:09 +0100

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes'
 
 class Quotes extends Component {
 
@@ -15,11 +16,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {this.props.quotes.map(quote => <QuoteCard key={quote.id} quote={quote} removeQuote={this.props.removeQuote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} />)}
             </div>
           </div>
         </div>
@@ -28,5 +25,4 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(state => ({quotes: state.quotes}), { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  let quote;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId)
+      idx = state.indexOf(quote);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+
+    case 'UPVOTE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId)
+      quote.votes += 1
+      return state;
+
+    case 'DOWNVOTE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId)
+      if (quote.votes > 0 ){quote.votes -= 1}
+      return state
+
+    default:
+      return state
+  }
 }
