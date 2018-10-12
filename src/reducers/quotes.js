58115713253501e978d33eb0920d9c04a6a7c593
Gingertonic <bethmschofield@gmail.com>
2018-10-12 10:37:32 +0100

    Fix render of vote change

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -10,17 +10,80 @@ export default (state = [], action) => {
       idx = state.indexOf(quote);
       return [...state.slice(0, idx), ...state.slice(idx + 1)]
 
-    case 'UPVOTE_QUOTE':
-      quote = state.find(quote => quote.id === action.quoteId)
-      quote.votes += 1
-      return state;
+    // case 'UPVOTE_QUOTE':
+    //   quote = state.find(quote => quote.id === action.quoteId)
+    //   quote.votes += 1
+    //   return state;
+
+  case 'UPVOTE_QUOTE':
+    idx = state.findIndex(quote => quote.id === action.quoteId);
+    quote = state[idx];
+
+    return [
+      ...state.slice(0, idx),
+      Object.assign({}, quote, { votes: quote.votes += 1 }),
+      ...state.slice(idx + 1)
+    ];
+    //
+    // case 'DOWNVOTE_QUOTE':
+    //   quote = state.find(quote => quote.id === action.quoteId)
+    //   if (quote.votes > 0 ){quote.votes -= 1}
+    //   return state
 
     case 'DOWNVOTE_QUOTE':
-      quote = state.find(quote => quote.id === action.quoteId)
-      if (quote.votes > 0 ){quote.votes -= 1}
-      return state
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[idx]
+
+      if (quote.votes > 0){
+        return [
+          ...state.slice(0, idx),
+          Object.assign({}, quote, {votes: quote.votes -= 1}),
+          ...state.slice(idx +1)
+        ]
+      }
 
     default:
       return state
   }
 }
+
+/// below is from solution
+
+// export default (state = [], action) => {
+//   let index;
+//   let quote;
+//
+//   switch (action.type) {
+//
+//     case 'ADD_QUOTE':
+//       return state.concat(action.quote);
+//
+//     case 'REMOVE_QUOTE':
+//       return state.filter(quote => quote.id !== action.quoteId);
+//
+//     case 'UPVOTE_QUOTE':
+//       index = state.findIndex(quote => quote.id === action.quoteId);
+//       quote = state[index];
+//
+//       return [
+//         ...state.slice(0, index),
+//         Object.assign({}, quote, { votes: quote.votes += 1 }),
+//         ...state.slice(index + 1)
+//       ];
+//
+//     case 'DOWNVOTE_QUOTE':
+//       index = state.findIndex(quote => quote.id === action.quoteId);
+//       quote = state[index];
+//       if (quote.votes > 0) {
+//         return [
+//           ...state.slice(0, index),
+//           Object.assign({}, quote, { votes: quote.votes -= 1 }),
+//           ...state.slice(index + 1)
+//         ];
+//       }
+//       return state;
+//
+//     default:
+//       return state;
+//   }
+// }
