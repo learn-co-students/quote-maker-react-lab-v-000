d08c947072ce1b99a34d56a3ebc336f462c89052
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-14 01:06:22 -0400

    Implement reducer behavior

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
+import { getMaxListeners } from "cluster";
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      return state.map(quote => {
+        quote.votes = (quote.id === action.quoteId) ? quote.votes + 1 : quote.votes
+        return quote
+      })
+
+    case 'DOWNVOTE_QUOTE':
+      return state.map(quote => {
+        quote.votes = (quote.id === action.quoteId) ?
+          Math.max(0, quote.votes - 1) : 
+          quote.votes
+        return quote
+      })
+
+    default:
+      return state;
+  }
 }
