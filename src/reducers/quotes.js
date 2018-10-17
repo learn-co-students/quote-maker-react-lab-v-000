649cfd5f4c90d802be67ae6faefce6fa7505d351
BarbaraPruz <barbara.prusiewicz@gmail>
2018-10-16 21:26:21 -0700

    Add actions and reducers

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [ ...state, action.quote];
+    case "REMOVE_QUOTE":
+      return state.filter( quote => quote.id !== action.id);
+    case "UPVOTE_QUOTE":
+      let idx = state.findIndex( (quote) => quote.id === action.id)
+      return state.map ( function (quote, index) {
+        if (index === idx) quote.votes++
+        return quote;
+      });
+    case "DOWNVOTE_QUOTE":
+      let idx = state.findIndex( (quote) => quote.id === action.id)
+      return state.map ( function (quote, index) {
+        if ((index === idx) && (quote.votes > 0))quote.votes--
+        return quote;
+      });
+    default:
+      return state;
+  }
+
 }
