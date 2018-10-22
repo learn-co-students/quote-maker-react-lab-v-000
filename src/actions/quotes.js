290c10d28f715c5e279056f161b9abc31b72b45b
nmlstyl <joey.michalina.mariano@gmail.com>
2018-10-22 17:03:33 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,27 @@
-// TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    quote,
+    type: 'ADD_QUOTE'
+  };
+};
+
+export const removeQuote = quoteId => {
+  return {
+    quoteId: quoteId,
+    type: 'REMOVE_QUOTE'
+  };
+};
+
+export const upvoteQuote = quoteId => {
+  return {
+    quoteId: quoteId,
+    type: 'UPVOTE_QUOTE'
+  };
+};
+
+export const downvoteQuote = quoteId => {
+  return {
+    quoteId: quoteId,
+    type: 'DOWNVOTE_QUOTE'
+  };
+};
