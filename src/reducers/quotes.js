193ce5d2e41e20b2a857ca498fac6c0560deab70
github username <github email address>
2018-10-24 19:48:09 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
 export default (state = [], action) => {
-  return state;
+  let index
+  let voteCount
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote]
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+      voteCount = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return {...quote, votes: ++quote.votes}
+        }
+      })
+      return voteCount
+
+    case "DOWNVOTE_QUOTE":
+      voteCount = state.map(quote => {
+        if (quote.id === action.quoteId && quote.votes > 0) {
+          return {...quote, votes: --quote.votes}
+        } else {
+          return quote
+        }
+      })
+      return voteCount
+
+    default:
+      return state;
+  }
 }
