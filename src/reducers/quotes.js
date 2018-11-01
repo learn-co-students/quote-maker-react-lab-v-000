f0d9a3dda3346bbf3505693e3f310b6cf06f57f0
nlisauskas <nlisausk@alumni.nd.edu>
2018-10-31 21:41:03 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteID)
+    case 'UPVOTE_QUOTE':
+      let index = state.findIndex(quote => quote.id === action.quoteID);
+      let quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, {votes: quote.votes += 1}),
+        ...state.slice(index + 1)
+      ];
+
+      case 'DOWNVOTE_QUOTE':
+  index = state.findIndex(quote => quote.id === action.quoteId);
+  quote = state[index];
+  if (quote.votes > 0) {
+    return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(index + 1)
+        ];
+                        }
+    return state;
+    default:
+      return state;
+  }
 }
