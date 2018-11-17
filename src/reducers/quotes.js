73a01e9ad9fa32a45f217229721a79e112bae05c
yutingcxiang <yutingcxiang@gmail.com>
2018-11-17 17:57:41 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,30 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+
+  switch (action.type){
+
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, index), Object.assign({}, state[index], {votes: state[index].votes += 1}), ...state.slice(index + 1)]
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId)
+      if (state[index].votes > 0){
+        return [...state.slice(0, index), Object.assign({}, state[index], {votes: state[index].votes -= 1}), ...state.slice(index + 1)]
+      } else {
+        return state;
+      }
+
+    default:
+      return state;
+  }
 }
