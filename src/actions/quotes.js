c407b582b4d059be58d00779d5e62b8e5ef46710
tsyaeger <tsyaeger@gmail.com>
2018-11-26 21:14:27 -0800

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,37 @@
 // TODO: Create action creators as defined in tests
+import uuid from 'uuid'
+
+
+// const id = uuid()
+
+
+export const addQuote = quoteId => {
+	return {
+		type: 'ADD_QUOTE',
+		quote
+	}
+}
+
+export const removeQuote = quoteId => {
+	return {
+		type: 'REMOVE_QUOTE',
+		quote
+	}
+}
+
+
+export const upvoteQuote = quoteId => {
+	return {
+		type: 'UPVOTE_QUOTE',
+		quote
+	}
+}
+
+
+
+export const downvoteQuote = quoteId => {
+	return {
+		type: 'DOWNVOTE_QUOTE',
+		quote
+	}
+}
