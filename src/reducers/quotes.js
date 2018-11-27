52e5b306250cc240e9be68c070375e1370bc95fc
tsyaeger <tsyaeger@gmail.com>
2018-11-26 20:50:11 -0800

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
-  return state;
+	let idx;
+
+	switch(action.type){
+		case 'ADD_QUOTE':
+			return {
+				...state, 
+				quotes: [...state.quotes, action.quote]
+			}
+		case 'REMOVE_QUOTE':
+			return {
+		        ...state,
+		        quotes: [state.quotes.slice(0, idx), state.quotes.slice(idx + 1)]
+			}
+		case 'UPVOTE':
+			return {
+
+			}
+		case 'DOWNVOTE':
+			return {
+
+			}
+		default:
+			return state;
+
+	}
 }
+
