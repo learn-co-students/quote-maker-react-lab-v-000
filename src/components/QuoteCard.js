aaeb9534e683a8ac93eea94cb4aff4e331f3962b
dwieb <dwayne.harmon@gmail.com>
2018-12-09 05:39:48 -0600

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
+const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) => 
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{quote.content}</p>
+          <footer>- author <cite title="Source Title">{quote.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
@@ -30,7 +30,7 @@ const QuoteCard = (props) =>
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {quote.votes}</div>
       </div>
     </div>
   </div>;
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      return [].concat(state, action.quote)
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id != action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      var upvotedIndex = state.findIndex(quote => quote.id === action.quoteId)
+      state[upvotedIndex].votes++
+      return state;
+
+    case 'DOWNVOTE_QUOTE':
+      var downvotedIndex = state.findIndex(quote => quote.id === action.quoteId)
+      if (state[downvotedIndex].votes > 0) {
+        state[downvotedIndex].votes--
+      }
+      return state;
+    default:
+      return state;
+  }
 }
