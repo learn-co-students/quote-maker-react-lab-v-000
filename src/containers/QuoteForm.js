0f3e8d12c172974372ef205b7465a4bdd97eaa66
pcherian10 <pcherian10@gmail.com>
2019-01-17 18:51:37 -0500

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -4,21 +4,30 @@ import { connect } from 'react-redux';
 import { addQuote } from '../actions/quotes';
 
 class QuoteForm extends Component {
-
-  state = {
-    //set up a controlled form with internal state
+  constructor(props) {
+    super(props)
+    this.state = {
+      content: '',
+      author: ''
+    }
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
+    event.preventDefault();
+    const quote = {...this.state, id: uuid(), votes:0 };
+    this.props.addQuote(quote);
+    this.setState({
+      content: '',
+      author: ''
+    });
   }
+
 
   render() {
     return (
@@ -27,13 +36,15 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
+                        name = "content"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -42,8 +53,10 @@ class QuoteForm extends Component {
                     <div className="col-md-5">
                       <input
                         className="form-control"
+                        name = "author"
                         type="text"
                         value={this.state.author}
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -62,5 +75,4 @@ class QuoteForm extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, {addQuote})(QuoteForm);
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import {removeQuote, upvoteQuote, downvoteQuote} from "../actions/quotes"
+import { bindActionCreators} from 'redux'
+
 
 class Quotes extends Component {
 
   render() {
+
+    const quotes = this.props.quotes.map((q,i) => {
+      return <QuoteCard key={i} quote={q} upvoteQuote={this.props.upvoteQuote}
+        downvoteQuote={this.props.downvoteQuote} removeQuote={this.props.removeQuote} />
+    });
+
     return (
       <div>
         <hr />
@@ -15,11 +24,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes}
             </div>
           </div>
         </div>
@@ -28,5 +33,17 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = state => {
+  return { quotes: state.quotes}
+}
+
+const mapStateToDispatch = dispatch => {
+  return bindActionCreators ({
+      removeQuote: removeQuote,
+      upvoteQuote: upvoteQuote,
+      downvoteQuote: downvoteQuote,
+    }, dispatch)
+}
+
+
+export default connect(mapStateToProps, mapStateToDispatch)(Quotes);
