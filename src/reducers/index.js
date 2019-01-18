ee8aab3f8a1df6989f17175f1b3506fec3f51e66
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 02:38:52 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
 import quotes from './quotes';
 
-export default combineReducers({
-  quotes
-});
+export default combineReducers({quotes});
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,30 @@
 export default (state = [], action) => {
+  let idx;
+  let updatedQuote;
+  
+  switch (action.type) {
+    
+    case "ADD_QUOTE":
+      return [...state, {...action.quote, votes: 0}]
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+     updatedQuote = state.find(quote => quote.id === action.quoteId)
+     idx = state.indexOf(updatedQuote)
+     ++updatedQuote.votes
+      return Object.assign([...state], {[idx]: updatedQuote})
+
+    case "DOWNVOTE_QUOTE":
+      updatedQuote = state.find(quote => quote.id === action.quoteId)
+      idx = state.indexOf(updatedQuote)
+    if (updatedQuote.votes > 0) {
+        --updatedQuote.votes
+      return Object.assign([...state], {[idx]: updatedQuote})
+    } 
+    
+  default:
   return state;
+  }
 }
