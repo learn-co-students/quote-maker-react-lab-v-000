28fa94393b0f5e4544564b3c59093b2a929ee417
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2019-01-30 14:17:01 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,27 @@
-// TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: "ADD_QUOTE",
+    quote: Object.assign({}, quote, { votes: 0 })
+  };
+};
+
+export const removeQuote = quote => {
+  return {
+    type: "REMOVE_QUOTE",
+    quoteId
+  };
+};
+
+export const upvoteQuote = quote => {
+  return {
+    type: "UPVOTE_QUOTE",
+    quoteId
+  };
+};
+
+export const downvoteQuote = quote => {
+  return {
+    type: "DOWNVOTE_QUOTE",
+    quoteId
+  };
+};
