470ad26752e0b75ccdf9657098c7c913472411b0
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2019-01-30 14:31:02 -0500

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import QuoteCard from '../components/QuoteCard';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import QuoteCard from "../components/QuoteCard";
+import { removeQuote, upvoteQuote, downvoteQuote } from "../actions/quotes";
+import quotes from "../reducers/quotes";
 
 class Quotes extends Component {
-
   render() {
     return (
       <div>
@@ -15,11 +16,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes.map(quote => <QuoteCard key={quote.id} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote} removeQuote={removeQuote} quote={quote} />)}
             </div>
           </div>
         </div>
