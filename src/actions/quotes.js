bd9c103e37ab2bff280cd31543a91b2173a74548
Jenny Kam <jkam17@gmail.com>
2019-03-03 18:03:38 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,28 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quoteObj) => {
+  return {
+    type: 'ADD_QUOTE',
+    quote: quoteObj
+  }
+}
+
+export const removeQuote = (qidObj) => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId: qidObj
+  }
+}
+
+export const upvoteQuote = (qidObj) => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId: qidObj
+  }
+}
+
+export const downvoteQuote = (qidObj) => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId: qidObj
+  }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  let obj
+  let votes
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      return [...state,action.quote]
+
+    case 'REMOVE_QUOTE':
+      return state.filter(obj => obj.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      obj = state.filter(obj => obj.id === action.quoteId)
+      votes = obj[0].votes
+      debugger
+      return [{...obj[0], votes: votes+1}]
+
+    case 'DOWNVOTE_QUOTE':
+      obj = state.filter(obj => obj.id === action.quoteId)
+      votes = (obj[0].votes - 1) < 0 ? 0 : (obj[0].votes - 1)
+      return [{...obj[0], votes: votes}]
+
+    default:
+      return state;
+  }
 }
