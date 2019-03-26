f134f4ac3cba8784b1bed22bf1e994f50054f4a0
Kkelly22 <kk504408@gmail.com>
2019-03-25 21:49:42 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { upvoteQuote } from '../actions/quotes';
+import { downvoteQuote } from '../actions/quotes';
+import { removeQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
+  onUpVote = quoteId => {
+    debugger
+    this.props.upvoteQuote(quoteId);
+  }
+
+  onDownVote = quoteId => {
+    this.props.downvoteQuote(quoteId);
+  }
+
+  onRemoveQuote = quoteId => {
+    this.props.removeQuote(quoteId);
+  }
+
   render() {
+    const quotes = this.props.quotes.map((quote, index) => <QuoteCard key={index} quote={quote} onUpVote={this.onUpVote} onDownVote={this.onDownVote} onRemoveQuote={this.onRemoveQuote} />);
+
     return (
       <div>
         <hr />
@@ -15,11 +33,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+            {quotes}
             </div>
           </div>
         </div>
@@ -29,4 +43,25 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+
+const mapStateToProps = state => {
+  debugger
+  return { quotes: state.quotes }
+}
+
+const mapDispatchToProps = dispatch => {
+  debugger
+  return {
+    upvoteQuote: (quoteId) => {
+      dispatch(upvoteQuote(quoteId))
+    },
+    downvoteQuote: (quoteId) => {
+      dispatch(downvoteQuote(quoteId))
+    },
+    removeQuote: (quoteId) => {
+      dispatch(removeQuote(quoteId))
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
 export default (state = [], action) => {
-  return state;
-}
+	let idx;
+ 
+	switch (action.type) {
+	
+	case 'ADD_QUOTE':
+      return [...state, { id: action.quote.id, content: action.quote.content, author: action.quote.author, votes: action.quote.votes }];
+
+    case "REMOVE_QUOTE":
+      idx = state.indexOf(action);
+      const newState = state
+      return newState.splice(idx, 1)
+
+    case 'UPVOTE_QUOTE':
+      idx = state.indexOf(action);
+      return state;
+
+    case 'DOWNVOTE_QUOTE':
+      idx = state.indexOf(action);
+      return [...state];
+
+ 
+    default:
+      return state;
+
+	}
+};
