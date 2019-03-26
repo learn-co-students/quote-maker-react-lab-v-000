bcf02dc26a0e6b92b774840cc3390f3606db910b
MiriamPeskowitz <miriam5000@gmail.com>
2019-03-26 15:43:17 -0400

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -22,10 +22,7 @@ class QuoteForm extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     //create quote object from state 
-    const quote = {...this.state,
-      // content: this.state.content, 
-      // author: this.state.author,
-      id: uuid()
+    const quote = {...this.state, id: uuid()
     };
   //could change votes to default
 
@@ -48,7 +45,9 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                
+                <form className="form-horizontal"
+                    onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
@@ -76,7 +75,9 @@ class QuoteForm extends Component {
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                      <button type="submit" 
+                          className="btn btn-default"
+                          >Add</button>
                     </div>
                   </div>
                 </form>
