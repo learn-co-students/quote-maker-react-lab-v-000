f2a7fe0a13fcbc18ee0b03993025d73cde33ad71
pracniscate <lorena.giddens@outlook.com>
2019-04-02 06:51:50 -0400

    done

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,41 @@
 export default (state = [], action) => {
-  return state;
+
+  let quote;
+  let index;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      // The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+      return state.concat(action.quote);
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(index + 1)
+        ];
+      }
+      return state;
+
+    default:
+      return state;
+  }
 }
