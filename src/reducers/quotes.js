63f01d135c675fd416b1cca2fa393410b2684740
terryblue99 <terryblue@comcast.net>
2019-04-03 14:17:57 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,20 +2,29 @@ export default (state = [] , action) => {
 
 	switch (action.type) {
 
-		case 'ADD_QUOTE':
-			
+		case 'ADD_QUOTE':		
 			return [...state, action.quote]
 
-		case 'REMOVE_QUOTE':
-			
+		case 'REMOVE_QUOTE':			
 			return state.filter(quote => quote.id !== action.quoteId)
 
-		// case 'UPVOTE_QUOTE':
+		case 'UPVOTE_QUOTE': 			
+			const upQuotes = state.map(quote => {
+				if (quote.id === action.quoteId) { quote.votes += 1 }
+				return quote	
+				}	
+			)			
+			return upQuotes
 
-		// 	return [...state, ]
+		case 'DOWNVOTE_QUOTE': 
+			const downQuotes = state.map(quote => {
+				if (quote.id === action.quoteId && quote.votes > 0) { quote.votes -= 1 }
+				return quote	
+				}	
+			)		
+			return downQuotes
 
 		default:
-
   			return state
 
 	}
