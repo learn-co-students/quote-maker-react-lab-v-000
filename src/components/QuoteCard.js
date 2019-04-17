39bbd4dc95d10cc96691a4e3ec528b28f222b9ef
github username <github email address>
2019-04-16 21:26:47 -0700

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -5,6 +5,7 @@ const QuoteCard = (props) =>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
+          
           {/* <p>{Render Quote Content}</p> */}
           {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
         </blockquote>
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,14 @@
-export default (state = [], action) => {
-  return state;
+export default(state = [], action) => {
+  let idx;
+
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+      case 'REMOVE_QUOTE':
+        return state.filter(quote => quote.id !== action.quoteId);
+
+    default:
+      return state;
+  }
 }
