add6147a35d39b9bb83e53530674553be09621e6
emanuelbierman <emanuel.bierman@gmail.com>
2019-04-23 13:26:12 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,13 +2,17 @@ export default (state = [], action) => {
   let index;
   switch (action.type) {
     case "ADD_QUOTE":
-      return state;
+      // return state.concat(action.quote);
+      return [ ...state, action.quote ];
 
     case "REMOVE_QUOTE":
-      return state;
+      return state.filter(quote => quote.id !== action.quoteId);
 
     case "UPVOTE_QUOTE":
-      return state;
+      let quote = state.filter(quote => quote.id === action.quoteId);
+      quote[0].votes += 1;
+      state = state.filter(quote => quote.id !== action.quoteId);
+      return state.concat(quote);
 
     case "DOWNVOTE_QUOTE":
       return state;
