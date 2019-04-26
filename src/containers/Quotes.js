6a8a42e2ba7b2978d4297ed371c96733c7373a8b
amitnyc83 <amit_nyc@live.com>
2019-04-26 14:05:25 -0400

    displays qoutes & allows up/down voting

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import {removeQuote, upvoteQuote, downvoteQuote} from '../actions/quotes';
 
 
 class Quotes extends Component {
@@ -16,11 +17,13 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+             {this.props.quotes.map(quote => {
+               <QuoteCard quote={quote} key={quote.id}
+                 removeQuote={this.props.removeQuote}
+                 upvoteQuote={this.props.upvoteQuote}
+                 downvoteQuote={this.props.downvoteQuote}
+                />
+              })}
             </div>
           </div>
         </div>
@@ -29,5 +32,7 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = state => {
+  return ({ quotes: state.quotes })
+}
+export default connect(mapStateToProps, {removeQuote, upvoteQuote, downvoteQuote})(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,29 @@
 export default (state = [], action) => {
-  return state;
+  let selectedQuote;
+  switch(action.type) {
+    case "ADD_QUOTE":
+    return [...state, action.quote];
+
+    case "REMOVE_QUOTE":
+    return state.filter(quote => quote.id !== action.quoteId);
+
+    case "UPVOTE_QUOTE":
+    selectedQuote = state.filter(quote => quote.id === action.quoteId);
+    selectedQuote[0].votes += 1;
+    state = state.filter(quote => quote.id !== action.quoteId);
+    return state.concat(selectedQuote);
+
+    case "DOWNVOTE_QUOTE":
+    selectedQuote = state.filter(quote => quote.id === action.quoteId);
+    if ( selectedQuote[0].votes > 0 ){
+     selectedQuote[0].votes -= 1;
+     state = state.filter(quote => quote.id !== action.quoteId);
+     return state.concat(selectedQuote);
+    } else {
+      return state;
+    }
+
+    default:
+    return state;
+  }
 }
