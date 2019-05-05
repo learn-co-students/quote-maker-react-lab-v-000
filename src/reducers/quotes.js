7f15d1ba22be1b162eb489962c10cb8c1cba0111
anthonygharvey <anthony@anthonygharvey.com>
2019-05-05 12:39:26 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,36 @@
+let index;
+let quote;
+
 export default (state = [], action) => {
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case "UPVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: (quote.votes += 1) }),
+        ...state.slice(index + 1)
+      ];
+
+    case "DOWNVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      let votes = quote.votes === 0 ? 0 : quote.votes - 1;
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes }),
+        ...state.slice(index + 1)
+      ];
+
+    default:
+      break;
+  }
+
   return state;
-}
+};
