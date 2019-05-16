1d80519e8692ac0ccf0c93fb6dc1aa63cde6da29
Tahseen Siddiqui <tahseensiddiqui@Tahseens-MacBook-Pro.local>
2019-05-16 14:21:06 -0700

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -5,8 +5,8 @@ const QuoteCard = (props) =>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{quote.content}</p>
+           <footer>- author <cite title="Source Title">{quote.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
@@ -14,23 +14,26 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
+            onClick= {()=>props.upVote(quote.id)}
           >
             Upvote
           </button>
           <button
             type="button"
             className="btn btn-secondary"
+            onClick= {()=>props.downVote(quote.id)}
           >
             Downvote
           </button>
           <button
             type="button"
             className="btn btn-danger"
+            onClick= {()=>props.removeQuote(quote.id)}
           >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {quote.votes}</div>
       </div>
     </div>
   </div>;
