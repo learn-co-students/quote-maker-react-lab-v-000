18c244d6bb7e9e2de824ad4ed2d4093ecd9217e4
Jayon Kim <jpkim921@gmail.com>
2019-05-20 17:39:00 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import QuoteForm from "./containers/QuoteForm";
+import Quotes from "./containers/Quotes";
+
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
+        <div
+          className="row title justify-content-center"
+          style={{ paddingTop: "12px" }}
+        >
           <h1>Quote Maker</h1>
         </div>
         <hr />
@@ -13,6 +19,8 @@ class App extends Component {
 
           Render QuoteForm and Quotes Components
          */}
+        <QuoteForm />
+        <Quotes />
       </div>
     );
   }
