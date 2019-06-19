9699765313896bf531ab4257b20517ac683e68da
carolgreene <carolgreene3@gmail.com>
2019-06-19 15:31:18 -0700

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,28 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  return {
+    type: 'ADD_QUOTE',
+    quote
+  }
+}
+
+export const removeQuote = (quote) => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId: quote
+  }
+}
+
+export const upvoteQuote = (quote) => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId: quote
+  }
+}
+
+export const downvoteQuote = (quote) => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId: quote
+  }
+}
