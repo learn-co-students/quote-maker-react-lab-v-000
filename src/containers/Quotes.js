cfe9288786c4d64ab61362e6e49954390546c1b3
harleyharl <harleymteden@gmail.com>
2019-06-24 07:33:19 -0700

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import uuid from 'uuid';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
+
 
 class Quotes extends Component {
 
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     content: '',
+  //     author: ''
+  //   }
+  // }
+
   render() {
+    console.log(this.props)
+    const { quotes, removeQuote, upvoteQuote, downvoteQuote } = this.props;
     return (
       <div>
         <hr />
@@ -15,11 +28,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {quotes.map(quote => <QuoteCard key={quote.id} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote} removeQuote={removeQuote} quote={quote} />)}
             </div>
           </div>
         </div>
@@ -28,5 +37,10 @@ class Quotes extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = state => {
+  return ({ quotes: state.quotes })
+}
+//
+// //add arguments to connect as needed
+// export default connect(mapStateToProps)(Quotes);
+export default connect(mapStateToProps, {removeQuote, upvoteQuote, downvoteQuote})(Quotes)
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux'
 import rootReducer from './reducers/index'
 
-let store = createStore(rootReducer)
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App store={store}/>
   </Provider>,
   document.getElementById('root')
 );
