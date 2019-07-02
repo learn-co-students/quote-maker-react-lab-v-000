0b5e29a316a7da61c03131bc3c977a7d5a4625d2
roitblatari <roitblatari@gmail.com>
2019-07-02 11:07:09 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import QuoteCard from '../components/QuoteCard';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import QuoteCard from "../components/QuoteCard";
+import { removeQuote, downvoteQuote, upvoteQuote } from "../actions/quotes";
 
 class Quotes extends Component {
-
   render() {
+    console.log(this.props);
+    const allQuotes = this.props.quotes.map(quote => (
+      <QuoteCard
+        key={quote.id}
+        removeQuote={this.props.removeQuote}
+        upvoteQuote={this.props.upvoteQuote}
+        downvoteQuote={this.props.downvoteQuote}
+        quote={quote}
+      />
+    ));
     return (
       <div>
         <hr />
@@ -15,11 +25,13 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
+              {/* {this.props.} */}
               {/*
                 TODO:
-
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+              {allQuotes}
+              {/* <QuoteCard removeQuote={this.props.removeQuote} /> */}
             </div>
           </div>
         </div>
@@ -28,5 +40,24 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return { quotes: state.quotes };
+};
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     removeQuote: quoteId => {
+//       dispatch(removeQuote(quoteId));
+//     },
+//     upvoteQuote: quoteId => {
+//       dispatch(upvoteQuote(quoteId));
+//     }
+//   };
+// };
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(
+  mapStateToProps,
+  { upvoteQuote, downvoteQuote, removeQuote }
+  // mapDispatchToProps
+)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,45 @@
+// import uuid from "uuid";
+// const id = uuid();
+
 export default (state = [], action) => {
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return state.concat(action.quote);
+
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case "UPVOTE_QUOTE":
+      console.log(action.quoteId);
+      // const quoteIndex = state.findIndex(quote => quote.id === action.quoteId);
+      // console.log("quoteIndex:", quoteIndex);
+      let newarr = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          quote.votes += 1;
+        }
+        return quote;
+      });
+      return newarr;
+    case "DOWNVOTE_QUOTE":
+      console.log(action.quoteId);
+      // const quoteIndex = state.findIndex(quote => quote.id === action.quoteId);
+      // console.log("quoteIndex:", quoteIndex);
+      let newarray = state.map(quote => {
+        if (quote.id === action.quoteId && quote.votes >= 1) {
+          quote.votes -= 1;
+        }
+        return quote;
+      });
+      return newarray;
+    // state.slice(quoteIndex - 1, 0, state[quoteIndex]);
+    // console.log("quoteIndex:", quoteIndex);
+    // // debugger;
+    // return state.concat(action.quote);
+
+    // console.log(state);
+
+    default:
+      state;
+  }
   return state;
-}
+};
