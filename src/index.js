5398193844cb15acca597ba64b2854b23783f6ff
NotoriousCottonball <gpianka1@gmail.com>
2019-07-10 09:12:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'
+/*
 import rootReducer from './reducers/index'
+*/
+import { combineReducers } from 'redux';
+import quotes from './reducers/quotes';
+
+const rootReducer = combineReducers({
+  quotes
+});
 
 let store = createStore(rootReducer)
 
