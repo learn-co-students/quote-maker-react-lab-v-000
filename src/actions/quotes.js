e1963eed2be7f1e1a4e7a810a9564d8907a00eec
sammy-brake <sammy.brake@gmail.com>
2019-07-11 16:05:10 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,24 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote)  => {
+
+}
+
+
+
+xport const removeQuote = (quoteId) => {
+
+}
+
+
+
+
+export const upvoteQuote = (quoteId) => {
+
+}
+
+
+export const downvoteQuote = (quoteId => {
+
+}
+
+
