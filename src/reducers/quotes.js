076f8e2297f07d3aa6591107c7a39d0371243b67
Mika88 <mikaspivak@gmail.com>
2019-07-11 19:51:04 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,31 @@
 export default (state = [], action) => {
-  return state;
+  let index, quote, newQuote, newState
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case 'REMOVE_QUOTE':
+      index = state.findIndex(i => i.id === action.quoteId)
+      return [...state.slice(0, index), ...state.slice(index + 1)];
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(i => i.id === action.quoteId)
+      quote = state.find(quote => quote.id === action.quoteId)
+      newQuote = {...quote, votes: quote.votes + 1}
+      newState = [...state]
+      newState.splice(index, 1, newQuote)
+      return newState
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(i => i.id === action.quoteId)
+      quote = state.find(quote => quote.id === action.quoteId)
+
+      if(quote.votes === 0){
+        return [...state]
+      } else {
+        newQuote = {...quote, votes: quote.votes - 1}
+        newState = [...state]
+        newState.splice(index, 1, newQuote)
+        return newState
+      }
+    default:
+      return state;
+  }
 }
