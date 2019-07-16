980b52707195bf0e1b1546e8f0f886d82cabbe50
sammy-brake <sammy.brake@gmail.com>
2019-07-16 09:50:48 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,11 +1,35 @@
 export default (state = [], action) => {
 
 switch(action.type) {
+  case 'ADD_QUOTE':
+    return [...state, action.quote]
 
+  case 'REMOVE_QUOTE':
+    return state.filter(quote => quote.id != action.quoteId )
+
+  case 'UPVOTE_QUOTE':
   
+    let newState = state.map(quote => {
+      if (quote.id === action.quoteId){
+        return {...quote, votes: ++quote.votes}
+      } else{
+        return quote 
+      }
+    })
+    return newState;
+
+    case 'DOWNVOTE_QUOTE':
+    let reduceState = state.map(quote => {
+      if (quote.id === action.quoteId && quote.votes > 0){
+        return {...quote, votes: --quote.votes}
+      } else{
+        return quote 
+      }
+    })
+    return reduceState;
+
+
+
+  default: return state;
 }
-
-
-
-  return state;
 }
