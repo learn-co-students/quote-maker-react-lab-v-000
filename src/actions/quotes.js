d20578ff12af4e285375566379c92bc722ad147b
Rachel Wyatt <rw126@evansville.edu>
2019-07-22 10:31:27 -0400

    add remove, upvote, and downvote actions to quotes.js

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -4,3 +4,24 @@ export const addQuote = quote => {
         quote
     }
 }
+
+export const removeQuote = quoteId => {
+    return {
+        type: 'REMOVE_QUOTE',
+        quoteId
+    }
+}
+
+export const upvoteQuote = quoteId => {
+    return {
+        type: 'UPVOTE_QUOTE',
+        quoteId
+    }
+}
+
+export const downvoteQuote = quoteId => {
+    return {
+        type: 'DOWNVOTE_QUOTE',
+        quoteId
+    }
+}
