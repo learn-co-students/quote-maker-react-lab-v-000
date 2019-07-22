b7b0c187e54624d9edbb9c92c5908f5d76dd4ab0
Rachel Wyatt <rw126@evansville.edu>
2019-07-22 10:45:22 -0400

    add reducer to handle ADD_QUOTE

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,11 @@
 export default (state = [], action) => {
-  return state;
+
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    
+    default: 
+      return state;
+  }
+
 }
