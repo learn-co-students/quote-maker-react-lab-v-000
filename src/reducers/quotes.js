e843a4aa4014d77782048204526f46351c83d446
Rachel Wyatt <rw126@evansville.edu>
2019-07-22 11:50:43 -0400

    add upvote and downvote handling to quotes.js

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,4 +1,5 @@
 export default (state = [], action) => {
+  let newState;
 
   switch(action.type) {
     case 'ADD_QUOTE':
@@ -6,6 +7,26 @@ export default (state = [], action) => {
     
     case 'REMOVE_QUOTE':
       return state.filter(quote => quote.id != action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      newState = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return {...quote, votes: ++quote.votes}
+        } else {
+          return quote
+        }
+      })
+      return newState;
+
+    case 'DOWNVOTE_QUOTE':
+      newState = state.map(quote => {
+          if (quote.id === action.quoteId && quote.votes > 0) {
+            return {...quote, votes: --quote.votes}
+          } else {
+            return quote
+          }
+        })
+      return newState;
 
     default: 
       return state;
