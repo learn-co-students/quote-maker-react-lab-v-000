96351b10aa3a7880a00cbeff160bfcd091acc454
FionaDL <fionadlapham@gmail.com>
2019-08-14 11:16:54 +0200

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -7,14 +7,18 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    id: " "
+    content: " "
+    author: " "
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    event.preventDefault()
+    this.setState({content: event.target.value})
   }
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
+    event.preventDefault()
     // Create quote object from state
     // Pass quote object to action creator
     // Update component state to return to default state
@@ -31,7 +35,7 @@ class QuoteForm extends Component {
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
-                      <textarea
+                      <textarea onChange={this.handleOnChange}
                         className="form-control"
                         value={this.state.content}
                       />
@@ -62,5 +66,13 @@ class QuoteForm extends Component {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addQuote: () => {
+      dispatch(addQuote())
+    }
+  };
+};
+
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, mapDispatchToProps)(QuoteForm);
