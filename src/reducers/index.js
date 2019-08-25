0af2b28ac3545e8874b77bf798c5fd3c1b373463
Ashley <ashleywong678@gmail.com>
2019-08-24 22:05:54 -0400

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { createStore } from 'redux'
 import quotes from './quotes';
 
 export default combineReducers({
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,8 @@
 export default (state = [], action) => {
+  switch (action.type){
+    case 'ADD_QUOTE':
+      console.log(action)
+      // state: [...state, store.dispach()]
+  }
   return state;
 }
