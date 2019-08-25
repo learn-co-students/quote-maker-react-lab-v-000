280dc981764d24affdcfdb898fd6b16e44fe4380
Wendy Calderon <wendycalderon@gmail.com>
2019-08-25 15:51:04 -0400

    Quotes Reducer handles REMOVE_QUOTE

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,6 +2,9 @@ export default (state = [], action) => {
     switch (action.type) {
       case "ADD_QUOTE":
         return state.concat([action.quote])
+      case "REMOVE_QUOTE":
+        const quoteToBeRemoved = state.findIndex(quote => quote.id === action.quoteId);
+        return [...state.slice(0, quoteToBeRemoved), ...state.slice(quoteToBeRemoved + 1)];
       default:
         return state;
     }
