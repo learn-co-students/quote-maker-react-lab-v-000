89b21558086499c14336a42b846734ce151a11ce
Wendy Calderon <wendycalderon@gmail.com>
2019-08-25 15:25:13 -0400

    Quotes Reducer handles ADD_QUOTE

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,8 @@
 export default (state = [], action) => {
-  return state;
+    switch (action.type) {
+      case "ADD_QUOTE":
+        return state.concat([action.quote])
+      default:
+        return state;
+    }
 }
