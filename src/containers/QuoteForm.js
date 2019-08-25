184c9bd686b9166351c68414769ca3e74c64d58e
Wendy Calderon <wendycalderon@gmail.com>
2019-08-25 17:55:26 -0400

    Set controlled form w/internal stated, add onSubmit and onChange to form, author and content

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -7,6 +7,8 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    content: '',
+    author: ''
   }
 
   handleOnChange = event => {
@@ -27,11 +29,12 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={(event) => this.handleOnSubmit(event)}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea name="content"
+                        onChange={(event) => this.handleOnChange(event)}
                         className="form-control"
                         value={this.state.content}
                       />
@@ -41,6 +44,7 @@ class QuoteForm extends Component {
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
                       <input name="author"
+                        onChange={(event) => this.handleOnChange(event)}
                         className="form-control"
                         type="text"
                         value={this.state.author}
