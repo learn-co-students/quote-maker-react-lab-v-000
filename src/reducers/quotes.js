e49e73b40d6336d0cd502f9e151f60acdee65b50
inkanimal <theatomproject@gmail.com>
2019-09-03 22:36:16 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,37 @@
+import Quotes from "../containers/Quotes";
+
 export default (state = [], action) => {
-  return state;
+  let newState;
+
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+      case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+      
+      case "UPVOTE_QUOTE":
+        newState = state.map(quote => {
+          if (quote.id === action.quoteId) {
+            return { ...quote, votes: quote.votes += 1 }
+          } else {
+            return quote;
+          }
+        })
+        return newState;
+
+        case "DOWNVOTE_QUOTE":
+          newState = state.map(quote => {
+            if (quote.id === action.quoteId && quote.votes > 0) {
+              return { ...quote, votes: quote.votes -= 1}
+            }else {
+              return quote;
+            }
+          })
+          return newState
+
+          default:
+            return state;
+  }
+  
 }
