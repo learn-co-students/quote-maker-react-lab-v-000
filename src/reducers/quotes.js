25a269d68522efd9c559a8a7cb214aceb0564947
christianavdm <christianavdm@gmail.com>
2019-09-24 13:50:56 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,11 +1,20 @@
-export default function manageQuotes(state = {
-  todos: [],
-}, action) {
+export default function manageQuotes(state = [], action) {
+  let idx; 
   switch (action.type) {
-    case 'ADD_TODO':
+    case 'ADD_QUOTE': 
+      return state.concat(action.quote);
 
-      return { todos: state.todos.concat(action.payload.text) };
-
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    
+    case 'UPVOTE_QUOTE':
+      let quote = state.find(quote => quote.id === action.quoteId);
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      let new_quote = {...quote, votes: quote.votes + 1}; 
+      let quotes = [...state.slice(0, idx), ...state.slice(idx + 1)];
+      return quotes.push(new_quote); 
+    
     default:
       return state;
   }
