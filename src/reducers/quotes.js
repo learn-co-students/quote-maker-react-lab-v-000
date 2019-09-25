684d5f1eada8d2f4920ab3f0ee69dc69430b6bab
Rafael Lopez <lpz.raf@gmail.com>
2019-09-24 21:44:49 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,44 @@
+import Quotes from "../containers/Quotes";
+
+let newState;
+
 export default (state = [], action) => {
-  return state;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+    
+   case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    
+    case 'UPVOTE_QUOTE':
+     newState = state.map(quote => {
+       if (quote.id === action.quoteId) {
+         return { ...quote, votes: ++quote.votes}
+       } else {
+         return quote;
+       }
+     })
+      return newState
+
+      case 'DOWNVOTE_QUOTE':
+          newState = state.map(quote => {
+            if (quote.id === action.quoteId && quote.votes > 0) {
+              return { ...quote, votes: --quote.votes}
+            } else {
+              return quote;
+            }
+          })
+           return newState
+   
+
+    default:
+      return state;
+  }
 }
+
+
+
+
+  
+    
