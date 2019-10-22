eef68d594720d4ea4c957b84b17928d2bb392410
github username <github email address>
2019-10-22 13:40:38 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,34 @@
 // TODO: Create action creators as defined in tests
+
+function addQuote(quote) {
+  return {
+    type: 'ADD_QUOTE',
+    quote: quote
+  }
+}
+export default addQuote;
+
+function removeQuote(quoteId) {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId: quoteId
+  }
+}
+export default removeQuote;
+
+function upvoteQuote(quoteId) {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId: quoteId
+  }
+}
+export default upvoteQuote;
+
+
+function downvoteQuote(quoteId) {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId: quoteId
+  }
+}
+export default downvoteQuote;
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,13 @@
+import quotes from '../actions/quotes'
+
 export default (state = [], action) => {
-  return state;
+  //return state;
+  let idx;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return addQuote();
+
+    default:
+      return state;
+  }
 }
