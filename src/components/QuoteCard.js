5e5c08a992516ecc5226c91faaafc6b0857a571a
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2019-10-29 22:42:27 -0400

    done

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+// import { upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 const QuoteCard = (props) =>
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
         <blockquote className="card-blockquote">
-          {/* <p>{Render Quote Content}</p> */}
-          {/* <footer>- author <cite title="Source Title">{Render Quote Author}</cite></footer> */}
+          <p>{props.quote.content}</p>
+          <footer>- author <cite title="Source Title">{props.quote.author}</cite></footer>
         </blockquote>
       </div>
       <div className="float-right">
@@ -14,12 +16,14 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
+            onClick={() => props.upvote(props.quote.id)}
           >
             Upvote
           </button>
           <button
             type="button"
             className="btn btn-secondary"
+            onClick={() => props.downvote(props.quote.id)}
           >
             Downvote
           </button>
@@ -30,9 +34,26 @@ const QuoteCard = (props) =>
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div>Votes: {Render Quote Votes}</div> */}
+        <div>Votes: {props.quote.votes}</div>
       </div>
     </div>
   </div>;
+
+  // const handleUpvote = function(id){
+    // console.log('upvoting');
+    // console.log(id)
+    // // this.props.dispatch(upvoteQuote(this.state));
+  //   this.props.dispatch(upvoteQuote(id));
+  // }
+
+  // const handleDownvote = (event) => {
+  //   console.log('downvoting');
+  //   // upvoteQuote();
+  // }
+
+  // const mapDispatchToProps = {
+  //   upvoteQuote,
+  //   downvoteQuote
+  // }
 
 export default QuoteCard;
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,41 @@
+import uuid from 'uuid';
+// const id = uuid();
+
 export default (state = [], action) => {
-  return state;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote];
+
+    case 'REMOVE_QUOTE':
+      let arr = [...state].filter(quote => quote.id !== action.quoteId);
+      return arr;
+
+    case 'UPVOTE_QUOTE':
+      let upvoted = state.map(quote => {
+        if (quote.id === action.quoteId){
+          return {...quote, ...quote.votes += 1}
+        } else {
+          return quote;
+        }
+      });
+      return upvoted;
+
+    case 'DOWNVOTE_QUOTE':
+      let downvoted = state.map(quote => {
+        if (quote.id === action.quoteId){
+          if (quote.votes > 0) {
+            return {...quote, ...quote.votes -= 1}
+          } else {
+            return quote;
+          }
+        } else {
+          return quote;
+        }
+      });
+      return downvoted;
+
+    default:
+        return state;
+  }
+  // return state;
 }
