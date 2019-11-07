19d0709195887f8b2a1f5345bb23273d18dd004a
Erin Green <erin.y.green@gmail.com>
2019-11-07 20:54:53 +0000

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,50 +1,58 @@
 import React from 'react';
 
-const QuoteCard = (props) =>
+const QuoteCard = props => {
 
-  dispatchAction = event => {
-    
-    this.props.dispatch
+  handleClick = event => {
+    event.preventDefault();
+    if (event.target.name === "upvote") {
+      props.dispatch.upvoteQuote(props.id)
+    } if (event.target.name === "downvote") {
+      props.dispatch.downvoteQuote(props.id)
+    } if (event.target.name === "delete") {
+      props.dispatch.removeQuote(props.id)
+    }
   }
 
-  <div>
-    <div className="card card-inverse card-success card-primary mb-3 text-center">
-      <div className="card-block">
-        <blockquote className="card-blockquote">
-          <p>{this.props.content}</p> 
-          <footer>- author <cite title="Source Title">{this.props.author}</cite></footer>
-        </blockquote>
-      </div>
-      <div className="float-right">
-        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
-            className="btn btn-primary"
-            name="upvote"
-            onClick={this.dispatchAction}
-          >
-            Upvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-secondary"
-            name="downvote"
-            onClick={this.dispatchAction}
-          >
-            Downvote
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger"
-            name="delete"
-            onClick={this.dispatchAction}
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
+  return (
+    <div>
+      <div className="card card-inverse card-success card-primary mb-3 text-center">
+        <div className="card-block">
+          <blockquote className="card-blockquote">
+            <p>{props.content}</p> 
+            <footer>- author <cite title="Source Title">{props.author}</cite></footer>
+          </blockquote>
         </div>
-        <div>Votes: {this.props.quote.votes}</div>
+        <div className="float-right">
+          <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              className="btn btn-primary"
+              name="upvote"
+              onClick={this.handleClick}
+            >
+              Upvote
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              name="downvote"
+              onClick={this.handleClick}
+            >
+              Downvote
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              name="delete"
+              onClick={this.handleClick}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div>Votes: {props.quote.votes}</div>
+        </div>
       </div>
-    </div>
-  </div>;
+    </div>)
+}
 
 export default QuoteCard;
