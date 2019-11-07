d5997f574dde2f28b517d6aa30e7345650786ad6
Erin Green <erin.y.green@gmail.com>
2019-11-07 19:35:35 +0000

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,29 @@
 // TODO: Create action creators as defined in tests
+
+export const addQuote = quote => {
+  return {
+    type: "ADD_QUOTE", 
+    quote 
+  }
+}
+
+export const removeQuote = quote => {
+  return {
+    type: "REMOVE_QUOTE", 
+    quote 
+  }
+}
+
+export const upvoteQuote = quote => {
+  return {
+    type: "UPVOTE_QUOTE", 
+    quote 
+  }
+}
+
+export const downvoteQuote = quote => {
+  return {
+    type: "DOWNVOTE_QUOTE", 
+    quote 
+  }
+}
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
-export default (state = [], action) => {
-  return state;
+export default function quotes(state = [], action) {
+ 
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return {
+        ...state,
+        books: [...state.books, action.book]
+      };
+
+    case "REMOVE_QUOTE":
+      return {
+        ...state,
+        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
+      };
+
+    case "UPVOTE_QUOTE":
+      return {
+        ...state,
+        authors: [...state.authors, action.author]
+      };
+
+    case "DOWNVOTE_QUOTE":
+      
+      return {
+        ...state,
+        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
+      };
+
+    default:
+      return state;
+  }
 }
