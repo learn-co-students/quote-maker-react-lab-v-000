64efcd4518549a6034ce4958b241d07957467409
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-12-01 15:03:38 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,24 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+	let idx;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return {...state, action };
+
+    case 'REMOVE_QUOTE':
+      idx = state.findIndex(quote => quote.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    //case 'UPVOTE QUOTE'
+
+     //return ;
+
+    //case 'DOWNVOTE QUOTE'
+
+    //return;
+
+    default:
+      return state;
+  }
 }
