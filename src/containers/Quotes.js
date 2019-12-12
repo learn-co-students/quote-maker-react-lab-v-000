55f14faa14f9f4015e84ede443ab8729b9ae580b
Bryan Hooper <bthooper@gmail.com>
2019-12-12 11:14:22 -0500

    update in progress

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -19,7 +19,8 @@ class Quotes extends Component {
                 TODO:
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+                */}
+                <QuoteCard />
             </div>
           </div>
         </div>
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      const id = uuid();
+      return [...state, { id: id, 
+        content: action.payload.content, 
+        author: action.payload.author
+      }];
+    case 'REMOVE_QUOTE':
+      return state;
+    case 'UPVOTE_QUOTE':
+      return state;
+    case 'DOWNVOTE_QUOTE':
+      return state;
+    default:
+      return state;
+  }
 }
