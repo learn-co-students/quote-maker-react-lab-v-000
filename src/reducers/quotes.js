844281041c65ecc77d44510244fb3dbd35015c8d
Heshie Brody <4hbrody@gmail.com>
2019-12-16 19:32:47 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,30 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
+    case 'REMOVE_QUOTE': {
+      let idx = state.findIndex(quote => action.quoteId === quote.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    }
+
+    case 'UPVOTE_QUOTE': {
+      let idx = state.findIndex(quote => action.quoteId === quote.id);
+      let quote = state[idx];
+      quote.votes += 1;
+
+      return [...state.slice(0, idx), quote, ...state.slice(idx + 1)];
+    }
+
+    case 'DOWNVOTE_QUOTE': {
+      let idx = state.findIndex(quote => action.quoteId === quote.id);
+      let prevVotes = state[idx].votes;
+      let newState = state[idx];
+      newState.votes = !prevVotes < 1 ? prevVotes - 1 : prevVotes;
+      return [...state.slice(0, idx), newState, ...state.slice(idx + 1)];
+    }
+
+    default:
+      return state;
+  }
 }
--- src/store.js
+++ src/store.js
@@ -3,9 +3,9 @@ import rootReducer from './reducers/index'
 
 export function configureStore(){
   return createStore(
-    rootReducer, 
+    rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+    )
 }
 
 export const store = configureStore()
