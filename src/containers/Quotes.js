f4dfdade93cd05fa071f4bf3466a821981dd276a
rgrier104 <rgrier104@gmail.com>
2019-12-17 14:03:37 -1000

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
+  renderQuotes = () => this.props.quotes.map((quote, id) => <QuoteCard key={id} quote={quote} removeQuote={this.props.removeQuote} upvoteQuote={this.props.upvoteQuote} downvoteQuote={this.props.downvoteQuote} />)
+  
   render() {
     return (
       <div>
@@ -15,11 +18,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {this.renderQuotes()}
             </div>
           </div>
         </div>
@@ -27,6 +26,11 @@ class Quotes extends Component {
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    quotes: state.quotes
+  }
+}
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      console.log(action.quote)
+      return [...state, action.quote];
+    
+    case 'REMOVE_QUOTE':
+      console.log(action.quoteId)
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+    
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[idx];
+      return [...state.slice(0, idx), 
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(idx + 1)];
+  
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[idx];
+      if (quote.votes > 0) {
+        return [...state.slice(0, idx),
+        Object.assign({}, quote, { votes: quote.votes -= 1 }),
+        ...state.slice(idx + 1)];
+      }
+      return state;
+    
+    default:
+      return state;
+  }
 }
