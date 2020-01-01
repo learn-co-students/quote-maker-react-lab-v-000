bac150d9041439a493fa51a5dcd0b8c43e650932
John Blank <veloblank@gmail.com>
2019-12-31 22:45:54 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,27 @@
-// TODO: Create action creators as defined in tests
+export function addQuote(quote) {
+  return {
+    type: "ADD_QUOTE",
+    quote: quote
+  };
+}
+
+export function removeQuote(id) {
+  return {
+    type: "REMOVE_QUOTE",
+    quoteId: id
+  };
+}
+
+export function upvoteQuote(id) {
+  return {
+    type: "UPVOTE_QUOTE",
+    quoteId: id
+  };
+}
+
+export function downvoteQuote(id) {
+  return {
+    type: "DOWNVOTE_QUOTE",
+    quoteId: id
+  };
+}
--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import uuid from 'uuid';
-import { connect } from 'react-redux';
-import { addQuote } from '../actions/quotes';
+import React, { Component } from "react";
+import uuid from "uuid";
+import { connect } from "react-redux";
+import { addQuote } from "../actions/quotes";
 
 class QuoteForm extends Component {
-
-  state = {
-    //set up a controlled form with internal state
+  constructor() {
+    super();
+    this.state = {
+      content: "",
+      author: ""
+    };
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
-  }
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
-  }
+    event.preventDefault();
+    let quote = { ...this.state, id: uuid(), votes: 0 };
+    this.props.addQuote(quote);
+    this.setState({
+      content: "",
+      author: ""
+    });
+  };
 
   render() {
     return (
@@ -27,29 +35,42 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form
+                  className="form-horizontal"
+                  onSubmit={this.handleOnSubmit}
+                >
                   <div className="form-group">
-                    <label htmlFor="content" className="col-md-4 control-label">Quote</label>
+                    <label htmlFor="content" className="col-md-4 control-label">
+                      Quote
+                    </label>
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
+                        name="content"
+                        onChange={this.handleOnChange}
                         value={this.state.content}
                       />
                     </div>
                   </div>
                   <div className="form-group">
-                    <label htmlFor="author" className="col-md-4 control-label">Author</label>
+                    <label htmlFor="author" className="col-md-4 control-label">
+                      Author
+                    </label>
                     <div className="col-md-5">
                       <input
                         className="form-control"
                         type="text"
+                        name="author"
+                        onChange={this.handleOnChange}
                         value={this.state.author}
                       />
                     </div>
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                      <button type="submit" className="btn btn-default">
+                        Add
+                      </button>
                     </div>
                   </div>
                 </form>
@@ -62,5 +83,10 @@ class QuoteForm extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(QuoteForm);
+const mapDispatchToProps = dispatch => {
+  return {
+    addQuote: quote => dispatch(addQuote(quote))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(QuoteForm);
