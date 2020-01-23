cef832cd2c2832d2d890e3933f57c3d8eb7e2b25
github username <github email address>
2020-01-22 22:18:04 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,7 +1,7 @@
 
 export default (state = [], action) => {
-  let index;
   let quote;
+
   switch(action.type) {
     case 'ADD_QUOTE':
     return state.concat(action.quote);
@@ -16,8 +16,7 @@ export default (state = [], action) => {
        {...quote, votes: quote.votes += 1}
     ]
     case 'DOWNVOTE_QUOTE':
-     index = state.findIndex(quote => quote.id === action.quoteId);
-     quote = state[index];
+     quote = state.find(quote => quote.id === action.quoteId);
 
      if(quote.votes > 0){
        return [
