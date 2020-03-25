07ab6ca2b4c8ea86a4e8013abf5684b847f3940c
github username <github email address>
2020-03-25 16:43:27 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,5 +1,6 @@
 export default (state = [], action) => {
   let idx;
+  let updatedState;
   switch (action.type) {
   case 'ADD_QUOTE':
     return [...state, action.quote];
@@ -7,17 +8,17 @@ export default (state = [], action) => {
     idx = state.findIndex(quote => quote.id  === action.quoteId)
     return [...state.slice(0, idx), ...state.slice(idx + 1)];
   case 'UPVOTE_QUOTE':
-    let updatedState = state.slice();
+    updatedState = state.slice();
     idx = updatedState.findIndex(quote => quote.id  === action.quoteId)
     updatedState[idx].votes += 1
     return updatedState
   case 'DOWNVOTE_QUOTE':
-    let updatedState2 = state.slice();
-    idx = updatedState2.findIndex(quote => quote.id  === action.quoteId)
-    if (updatedState2[idx].votes !== 0) {
-      updatedState2[idx].votes -= 1
+    updatedState = state.slice();
+    idx = updatedState.findIndex(quote => quote.id  === action.quoteId)
+    if (updatedState[idx].votes !== 0) {
+      updatedState[idx].votes -= 1
     }
-    return updatedState2
+    return updatedState
   default:
     return state;
   }
