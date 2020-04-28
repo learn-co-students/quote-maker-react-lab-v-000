87a1fe160b011973c00c4151d0e97ce5115bdcc5
Rebecca Tong <rtong@buffalo.edu>
2020-04-27 22:10:37 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -17,7 +17,7 @@ class Quotes extends Component {
             <div className="col-md-4">
 
 
-                  {this.props.quotes.map(quote => <QuoteCard quote={quote}/>)}
+                  {this.props.quotes.map(quote => <QuoteCard quote={quote} removeQuote={removeQuote(quote.id)} upvote={this.upvoteQuote(quote.id)} downvote={this.downvoteQuote(quote.id)}/>)}
                 // Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
 
 
@@ -38,16 +38,16 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    removeQuote: () => {
-      dispatch(removeQuote())
+    removeQuote: (quoteId) => {
+      dispatch(removeQuote(quoteId))
     },
 
-      upvoteQuote: () => {
-        dispatch(upvoteQuote())
+      upvoteQuote: (quoteId) => {
+        dispatch(upvoteQuote(quoteId))
       },
 
-      downvoteQuote: () => {
-        dispatch(downvoteQuote())
+      downvoteQuote: (quoteId) => {
+        dispatch(downvoteQuote(quoteId))
       }
 
   };
