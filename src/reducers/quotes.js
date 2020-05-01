bbd2b79afa706ddb1ab82e091c66e73a078bdb56
nanton83 <nicholas.romans@gmail.com>
2020-05-01 17:48:49 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -5,7 +5,7 @@ export default (state = [], action) => {
   switch (action.type) {
 
     case 'ADD_QUOTE':
-      return state.concat(action.quote);
+      return [...state, action.quote]
 
     case 'REMOVE_QUOTE':
       return state.filter(quote => quote.id !== action.quoteId);
