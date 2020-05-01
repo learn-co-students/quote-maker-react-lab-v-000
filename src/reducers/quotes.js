3eea7debd9e2b564d0873d8db8452a4901294e24
nanton83 <nicholas.romans@gmail.com>
2020-05-01 18:01:42 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,6 +1,7 @@
 export default (state = [], action) => {
   let index;
   let quote;
+  let newState;
 
   switch (action.type) {
 
@@ -11,28 +12,26 @@ export default (state = [], action) => {
       return state.filter(quote => quote.id !== action.quoteId);
 
     case 'UPVOTE_QUOTE':
-      index = state.findIndex(quote => quote.id === action.quoteId);
-      quote = state[index];
-
-      return [
-        ...state.slice(0, index),
-        Object.assign({}, quote, { votes: quote.votes += 1 }),
-        ...state.slice(index + 1)
-      ];
+      newState = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return { ...quote, votes: ++quote.votes }
+        } else {
+          return quote;
+        }
+      })
+      return newState;
 
     case 'DOWNVOTE_QUOTE':
-      index = state.findIndex(quote => quote.id === action.quoteId);
-      quote = state[index];
-      if (quote.votes > 0) {
-        return [
-          ...state.slice(0, index),
-          Object.assign({}, quote, { votes: quote.votes -= 1 }),
-          ...state.slice(index + 1)
-        ];
-      }
-      return state;
+      newState = state.map(quote => {
+        if (quote.id === action.quoteId && quote.votes > 0) {
+          return { ...quote, votes: --quote.votes }
+        } else {
+          return quote;
+        }
+      })
+      return newState;
 
-    default: 
+    default:
       return state;
   }
 }
