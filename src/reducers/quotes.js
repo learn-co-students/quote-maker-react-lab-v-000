ee9b4add339e70a60a3d6c10804527ab2da78d3f
Blake A. Long <blake.a.long@gmail.com>
2020-05-07 10:26:42 -0600

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,6 +1,7 @@
 export default (state = [], action) => {
   let quotes
   let quote
+  let idx
   
   switch (action.type) {
     case 'ADD_QUOTE':
@@ -12,18 +13,24 @@ export default (state = [], action) => {
       return state.filter(quote => quote.id !== action.quoteId) 
     
     case 'UPVOTE_QUOTE':
-      console.log('upvote quote', action.quoteId)
-      quotes = [ ...state ]
-      quote = quotes.find(quote => quote.id === action.quoteId)
-      quote.votes += 1
-      return quotes
+      console.log('upvote quote', action)
+      // quotes = [ ...state ]
+      // quote = quotes.find(quote => quote.id === action.quoteId)
+      // quote.votes += 1
+      idx = state.findIndex(quote => quote.id === action.quoteId )
+      quote = state.find(quote => quote.id === action.quoteId )
+      console.log(quote)
+      return [...state.slice(0, idx), { ...quote, votes: quote.votes += 1 }, ...state.slice(idx + 1) ]
       
-    case 'DOWNVOTE_QUOTE':
-      console.log('downvote quote', action.quoteId)
-      quotes = [ ...state ]
-      quote = quotes.find(quote => quote.id === action.quoteId)
-      quote.votes = quote.votes > 0 ? quote.votes -= 1 : quote.votes
-      return quotes
+      case 'DOWNVOTE_QUOTE':
+        idx = state.findIndex(quote => quote.id === action.quoteId )
+        quote = state.find(quote => quote.id === action.quoteId )
+        let { votes } = quote
+        console.log('downvote quote', action.quoteId)
+        // quotes = [ ...state ]
+        // quote = quotes.find(quote => quote.id === action.quoteId)
+        votes = votes > 0 ? votes -= 1 : votes
+      return [...state.slice(0, idx), { ...quote, votes: votes }, ...state.slice(idx + 1) ]
     
     default:
       return state;
