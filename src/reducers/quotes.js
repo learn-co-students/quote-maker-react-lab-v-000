baa82f55ff5aa3b47278d96ab6919cbd7f6f6dd7
Sheila O'Donnell <sheilacodonnell@gmail.com>
2020-05-18 14:50:09 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,26 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, 
+        { id: action.quote.id, content: action.quote.content, author: action.quote.author, votes: action.quote.votes} 
+      ];
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      const upvote = state.find(quote => quote.id === action.quoteId)
+      upvote.votes += 1
+      return [upvote]
+
+    case 'DOWNVOTE_QUOTE':
+      const downvote = state.find(quote => quote.id === action.quoteId)
+      if (downvote.votes > 0) {
+        downvote.votes -= 1
+      }
+      return [downvote]
+      
+    default:
+      return state;
+  }
 }
