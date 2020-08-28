0bcff1c48698b1f36e5556102b80b900df250144
sdcro <sdcrouse91@gmail.com>
2020-08-28 00:53:29 -0700

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
-import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes.js';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
   
