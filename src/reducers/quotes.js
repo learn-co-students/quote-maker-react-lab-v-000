5fdbfd1869ee2f1057d280b5008c65d16cc12a44
jamieberrier <berrier.jamie@gmail.com>
2020-09-04 18:38:18 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  let quote;
+  
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+ 
+    case "REMOVE_QUOTE":
+      idx = state.findIndex(quote => quote.id === action.quoteId);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+ 
+    case "UPVOTE_QUOTE":
+      idx = state.find(quote => quote.id === action.quoteId);
+      idx.votes += 1
+      return [...state]
+
+    case "DOWNVOTE_QUOTE":
+      idx = state.find(quote => quote.id === action.quoteId);
+      if (idx.votes > 0) {
+        idx.votes -= 1
+      }
+      return [...state]
+
+    default:
+      return state;
+  }
 }
