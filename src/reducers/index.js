dfa26b37700bf63ab17b97ff73dd350f810f783f
KimMead <kimmeadows8@gmail.com>
2021-01-03 18:07:45 -0600

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import quotes from './quotes';
 
+import uuid from 'uuid';
+ 
+const id = uuid();
+console.log(id)
+
 export default combineReducers({
   quotes
 });
