3eaa7d506b7bbdaa5330c01e0365ef3e03a694ad
github username <github email address>
2021-06-08 00:58:34 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
 
-class App extends Component {
+import React, { Component } from 'react';
+import QuoteForm from './components/QuoteForm';
+import Quotes from './containers/Quotes';
+
+export class App extends Component {
   render() {
     return (
       <div className="container-fluid">
@@ -8,11 +11,8 @@ class App extends Component {
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        <QuoteForm />
+        <Quotes />
       </div>
     );
   }
