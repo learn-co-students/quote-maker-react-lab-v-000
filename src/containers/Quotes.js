842b57d17e1f8f86d3936150c201dff82a8ae6dd
emanao <emanao@me.com>
2021-06-10 13:46:30 -0400

    Render QuoteCard Component with quote data from the store.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -15,11 +15,7 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+                {this.props.quotes.map(quote=><QuoteCard key={quote.id} quote={quote}/>)}
             </div>
           </div>
         </div>
@@ -29,4 +25,6 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+
+// const mapStateToProps=(state)=>({quotes: state.quotes})
+export default connect(({quotes})=>({quotes}))(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,9 +2,13 @@ import uuid from 'uuid'
 export default (state = [], action) => {
     switch(action.type){
         case "ADD_QUOTE":
-
-            // console.log(action);
-            return state.concat({...action.quote, id: uuid() })
+            let quoteInit= {...action.quote, votes:0}
+            // add id property to quote if it doesnt have one already (tests suite create ids for quote obj)
+            let quote = !action.quote.id? {...quoteInit, id:uuid()}: quoteInit;
+            return state.concat(quote);
+        case "REMOVE_QUOTE":
+            console.log(state);
+             return state.filter((quote)=>quote.id !== action.quote.id);
         default:
             return state;
     }
